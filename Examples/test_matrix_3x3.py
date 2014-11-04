a1c6f571f622497ae998d051aa942e6bf369dbb2
hainm <hainm.comp@gmail.com>
2014-11-04 18:11:16 -0500

    add

--- Examples/test_matrix_3x3.py
+++ Examples/test_matrix_3x3.py
@@ -10,8 +10,15 @@ y = np.array([100.,]).astype(float)
 n = M3x3(y)
 n.Print("3x3 matrix n: ")
 
-mcp = m.copy()
+mcp = M3x3()
 
-print mcp == m
-
+#copy matrix "m" to mcp
+mcp.copy(m)
 mcp.Print("mcp matrix")
+
+print "mcp is equal to m? ", mcp == m
+
+#test Set diagonal
+mdig = M3x3(np.array([1., 2., 3.]))
+mdig.Print("diagonal matrix: ")
+
