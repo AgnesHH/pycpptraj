a6040eb6ba44e86000458e5a302e3ee60503097d
hainm <hainm.comp@gmail.com>
2014-11-26 16:14:54 -0500

    updated ./examples/test_TopologyList.py

--- examples/test_TopologyList.py
+++ examples/test_TopologyList.py
@@ -3,4 +3,5 @@ from TopologyList import TopologyList
 tl = TopologyList()
 tl.AddParmFile("./test_PyCpptraj/Tc5b.ff99SB_JSC.mb3.newHmass.dt4fs.top")
 tl.List()
-tl.GetParm_test(0)
+
+newtop = tl.GetParm_test(0)
