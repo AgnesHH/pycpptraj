cac66ab8b731364ce5933b530d43a803a3f960cd
hainm <hainm.comp@gmail.com>
2014-11-27 01:43:19 -0500

    add examples/test_Topology.py

--- examples/test_Topology.py
+++ examples/test_Topology.py
@@ -9,3 +9,15 @@ f = FileName(fname)
 top1.SetParmName("top1", f)
 print top1.ParmName
 top1.Summary()
+
+print "test atom_iterator"
+for atom in top1.atom_generator():
+    pass
+
+print "test res_iterator"
+for res in top1.res_generator():
+    pass
+
+print "test mol_iterator"
+for mol in top1.mol_generator():
+    pass
