9d08ea6a9b301e029db40dd3b5c977766ffa823b
hainm <hainm.comp@gmail.com>
2014-12-03 17:48:15 -0500

    update test_TopologyList.py

--- examples/test_TopologyList.py
+++ examples/test_TopologyList.py
@@ -5,4 +5,5 @@ tl.AddParmFile("./test_PyCpptraj/Tc5b.ff99SB_JSC.mb3.newHmass.dt4fs.top")
 tl.AddParmFile("./test_PyCpptraj/HP36.ff99SB_JSC.mb3.top")
 tl.List()
 
-newtop = tl.GetParm(0)
+t = tl.GetParm(1)
+t.Summary()
