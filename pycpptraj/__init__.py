614881d3a448f81ce7e4b1699433a46d1676cc3f
hainm <hainm.comp@gmail.com>
2014-12-11 04:27:20 -0500

    update __init__.py

--- pycpptraj/__init__.py
+++ pycpptraj/__init__.py
@@ -1,4 +1,14 @@
 """
 pycpptraj
 """
-from pycpptraj.Topology import Topology
+import os
+
+try:
+    amber_home = os.environ['AMBERHOME']
+except:
+    raise EnvironmentError("must set AMBERHOME")
+
+try:
+    from pycpptraj.Topology import Topology
+except:
+    raise EnvironmentError("make sure to set LD_LIBRARY_PATH to $AMBERHOME/lib")
