75681835b6bb2bfa12c9fb99b2ff14c4fe7aec4a
hainm <hainm.comp@gmail.com>
2014-12-12 17:05:24 -0500

    add

---
+++ pycpptraj/DataSet_1D.pyx
@@ -0,0 +1,30 @@
+# distutils: language = c++
+
+
+cdef class DataSet_1D:
+    def __cinit__(self):
+        pass
+        #self.thisptr = new _DataSet_1D()
+
+    def __dealloc__(self):
+        #del self.thisptr
+        pass
+
+    #def DataSet_1D(self):
+
+    #def DataSet_1D(self,DataSet:
+
+    #def  bint IsTorsionArray(self):
+
+    #def double Avg(self):
+
+    #def double Avg(self,double sd):
+
+    #def double Min(self):
+
+    #def double Max(self):
+
+    #def int CrossCorr(self, DataSet_1D, DataSet_1D, int, bint, bint):
+
+    #def double CorrCoeff(self, DataSet_1D):
+
