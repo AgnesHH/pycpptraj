16a5f47f506ace7f3fdb0541d1fe868f8b907669
hainm <hainm.comp@gmail.com>
2014-12-14 00:30:18 -0500

    update install

---
+++ for_travis/setup.py
@@ -0,0 +1,68 @@
+import os
+#import shutil
+#from glob import glob
+from distutils.core import setup
+from distutils.extension import Extension
+import Cython.Distutils.build_ext
+from Cython.Build import cythonize
+
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+rootname = os.getcwd()
+pycpptraj_home = rootname + "/pycpptraj/"
+action_dir = pycpptraj_home + "/Action/"
+ana_dir = pycpptraj_home + "/Analysis/"
+cpptraj_dir = cpptraj_include = rootname + "/cpptraj/"
+libdir = cpptraj_dir + "/lib/"
+
+# get *.pyx files
+pyxfiles = []
+with open("PYXLIST.txt", 'r') as f:
+    for line in f.readlines():
+        if "#" not in line:
+            pyxfiles.append(line.split("\n")[0])
+
+USE_PYX = True
+if not USE_PYX:
+    ext = ".cpp"
+else:
+    ext = ".pyx"
+
+ext_modules = []
+for ext_name in pyxfiles:
+    if "/" in ext_name:
+        ext_name = ext_name.replace("/", ".")
+    pyxfile = pycpptraj_home + ext_name + ext
+    extmod = Extension("pycpptraj." + ext_name,
+                    [pyxfile],
+                    libraries=['cpptraj'],
+                    language='c++',
+                    library_dirs=[libdir,],
+                    include_dirs=[cpptraj_include, pycpptraj_home, action_dir, ana_dir])
+    ext_modules.append(extmod)
+
+setup(
+    name="PyCpptraj",
+    version="0.1",
+    author="Hai Nguyen",
+    author_email="hainm.comp@gmail.com",
+    url="https://github.com/hainm/pycpptraj",
+    packages=['pycpptraj',],
+    description="""Python wrapper for cpptraj""",
+    long_description=read("README.md"),
+    license = "BSD License",
+    platform = "I don't know yet",
+    classifiers=[
+                'Development Status :: Beta',
+                'Operating System :: Linux',
+                'Intended Audience :: Science/Research',
+                'License :: BSD License',
+                'Programming Language :: Python',
+                'Programming Language :: Cython',
+                'Programming Language :: C',
+                'Programming Language :: C++',
+                'Topic :: Scientific/Engineering'],
+    ext_modules = ext_modules,
+    cmdclass = {'build_ext': Cython.Distutils.build_ext},
+)
