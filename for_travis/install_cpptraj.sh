b8d4af63c724e61357e90933429cf70f62eaab01
hainm <hainm.comp@gmail.com>
2014-12-13 22:04:43 -0500

    update Makefile

--- for_travis/install_cpptraj.sh
+++ for_travis/install_cpptraj.sh
@@ -5,4 +5,9 @@ git clone https://github.com/mojyt/cpptraj
 cd cpptraj
 export CPPTRAJHOME=`pwd`
 mkidr lib
-
+wget https://raw.githubusercontent.com/hainm/pycpptraj/master/for_travis/Makefile
+wget https://raw.githubusercontent.com/hainm/pycpptraj/master/for_travis/configure
+mv Makefile ./src/
+./configure -nobzlib  -nonetcdf -nobzlib  -nomathlib -nozlib -shared gnu
+cd ./src
+make libcpptraj.so
