e1f6746695842bb56d56d830c9ed2410c133cabd
hainm <hainm.comp@gmail.com>
2014-12-13 22:17:14 -0500

    update

--- for_travis/install_cpptraj.sh
+++ for_travis/install_cpptraj.sh
@@ -8,6 +8,6 @@ mkdir lib
 wget https://raw.githubusercontent.com/hainm/pycpptraj/master/for_travis/Makefile
 wget https://raw.githubusercontent.com/hainm/pycpptraj/master/for_travis/configure_pycpptraj
 mv Makefile ./src/
-./configure_pycpptraj -nobzlib  -nonetcdf -nobzlib  -nomathlib -nozlib -shared gnu
+sh ./configure_pycpptraj -nobzlib  -nonetcdf -nobzlib  -nomathlib -nozlib -shared gnu
 cd ./src
 make libcpptraj.so
