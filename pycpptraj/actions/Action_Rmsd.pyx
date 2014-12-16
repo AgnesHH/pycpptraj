fc082f060f67a0e454586b7921275fcbef83b7f5
hainm <hainm.comp@gmail.com>
2014-12-16 12:47:00 -0500

    update

--- pycpptraj/actions/Action_Rmsd.pyx
+++ pycpptraj/actions/Action_Rmsd.pyx
@@ -8,14 +8,25 @@ cdef class Action_Rmsd (Action):
         self.ptr = <_Action_Rmsd*> self.thisptr
 
     def __dealloc__(self):
-        del self.thisptr
+        if self.thisptr is not NULL:
+            del self.thisptr
 
-    @classmethod
-    def alloc(cls):
-        cdef DispatchObject dpobject = DispatchObject()
-        dpobject.thisptr[0] = deref(self.ptr.Alloc())
-        return dpobject
+    #def alloc(cls):
+    #    cdef DispatchObject dpobject = DispatchObject()
+    #    dpobject.thisptr[0] = deref(self.ptr.Alloc())
+    #    return dpobject
 
-    @classmethod
-    def help(cls):
-        self.ptr.Help()
+    #def help(cls):
+    #    self.ptr.Help()
+
+    #@classmethod
+    #def alloc(cls):
+    # got "Invalid operand type for '*' (Python object)"
+    # deref(cls.ptr.Alloc())
+    #    cdef DispatchObject dpobject = DispatchObject()
+    #    dpobject.thisptr[0] = deref(cls.ptr.Alloc())
+    #    return dpobject
+
+    #@classmethod
+    #def help(cls):
+    #    cls.ptr.Help()
