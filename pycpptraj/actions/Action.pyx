a0ef107aaa66a1ebbbcacf13a2cc07c4398333d7
hainm <hainm.comp@gmail.com>
2014-12-16 12:19:45 -0500

    moving file

--- pycpptraj/actions/Action.pyx
+++ pycpptraj/actions/Action.pyx
@@ -1,4 +1,23 @@
-class Action:
-    def __init__(self):
+# distutils: language = c++
+
+
+cdef class Action:
+    def __cinit__(self):
+        # don't directly create instance of this ABC class.
+        pass
+        #self.thisptr = new _Action()
+
+    def __dealloc__(self):
+        # should I del pointe here or in subclass? 
+        #del self.thisptr
         pass
 
+    def Init(self, ArgList argIn, TopologyList toplist, FrameList flist, DataSetList dslist, DataFileList dflist, int debug):
+        return self.thisptr.Init(argIn.thisptr[0], toplist.thisptr, flist.thisptr, dslist.thisptr, dflist.thisptr,
+                debug)
+
+    #def Setup(_Topology *, _Topology * *):
+    #    pass
+
+    #def Do_Action(int, _Frame *, _Frame * *):
+    #    pass
