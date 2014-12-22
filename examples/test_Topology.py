49e23cade9147d80a3b7a03bfe986de60705e631
hainm <hainm.comp@gmail.com>
2014-12-22 14:00:34 -0500

    update examples

--- examples/test_Topology.py
+++ examples/test_Topology.py
@@ -1,32 +1,38 @@
 import os
+import unittest
 from pycpptraj.Topology import Topology
 from pycpptraj.AtomMask import AtomMask
 from pycpptraj.FileName import FileName
 
-datadir = os.environ['PYCPPTRAJ_HOME'] + "/examples/data/"
-fname = "./data/Tc5b.top"
+class TestTopology(unittest.TestCase):
+    def test_1(self):
+        datadir = os.environ['PYCPPTRAJ_HOME'] + "/examples/data/"
+        fname = "./data/Tc5b.top"
+        
+        top = Topology(fname)
+        #top2 = top.modify_state_by_mask(AtomMask("!@CA"))
+        #
+        top.summary()
+        top.atom_info("@CA")
+        
+        #print "test atom_iterator"
+        #for atom in top.atom_generator():
+        #    pass
+        #
+        #print "test res_iterator"
+        #for res in top.res_generator():
+        #    pass
+        #
+        #print "test mol_iterator"
+        #for mol in top.mol_generator():
+        #    pass
+        
+        #top.print_atom_info("@CA")
+        
+        # dummy test for modify top
+        # test empty file
+        #top2 = Topology()
+        #top2.summary()
 
-top = Topology(fname)
-#top2 = top.modify_state_by_mask(AtomMask("!@CA"))
-#
-top.summary()
-top.atom_info("@CA")
-
-#print "test atom_iterator"
-#for atom in top.atom_generator():
-#    pass
-#
-#print "test res_iterator"
-#for res in top.res_generator():
-#    pass
-#
-#print "test mol_iterator"
-#for mol in top.mol_generator():
-#    pass
-
-#top.print_atom_info("@CA")
-
-# dummy test for modify top
-# test empty file
-#top2 = Topology()
-#top2.summary()
+if __name__ == "__main__":
+    unittest.main()
