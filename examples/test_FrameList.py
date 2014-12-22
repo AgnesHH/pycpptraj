52f2d4f25f33aadf242bbfc86ead00ec2a1eecbc
hainm <hainm.comp@gmail.com>
2014-12-22 14:08:43 -0500

    update examples, add unittest

--- examples/test_FrameList.py
+++ examples/test_FrameList.py
@@ -1,4 +1,5 @@
 import os
+import unittest
 from pycpptraj.Topology import Topology
 from pycpptraj.ArgList import ArgList
 from pycpptraj.Frame import Frame
@@ -6,25 +7,30 @@ from pycpptraj.AtomMask import AtomMask
 from pycpptraj.FrameList import FrameList
 from pycpptraj.TopologyList import TopologyList 
 
-datadir = os.environ['PYCPPTRAJ_HOME'] + "/examples/data/"
-toplist = TopologyList()
-toplist.add_parm_file("./data/Tc5b.top")
-top = toplist.get_parm(0)
-arglist = ArgList("./data/Tc5b.nat.crd")
-print "===================="
-print "toplist.info()"
-toplist.info()
-print "===================="
-try:
-    top.n_atoms = 3000
-except:
-    print "can't not set n_atoms"
-print "===================="
+class TestFrameList(unittest.TestCase):
+    def test_1(self):
+        datadir = os.environ['PYCPPTRAJ_HOME'] + "/examples/data/"
+        toplist = TopologyList()
+        toplist.add_parm_file("./data/Tc5b.top")
+        top = toplist.get_parm(0)
+        arglist = ArgList("./data/Tc5b.nat.crd")
+        print "===================="
+        print "toplist.info()"
+        toplist.info()
+        print "===================="
+        try:
+            top.n_atoms = 3000
+        except:
+            print "can't not set n_atoms"
+        print "===================="
+        
+        #mylist = FrameList()
+        #mylist.add_ref_frame(arglist, toplist)
+        #aref = mylist.get_active_reference()
+        #mylist.list()
+        #print mylist.n_frames
+        #
+        #print aref.xyz
 
-#mylist = FrameList()
-#mylist.add_ref_frame(arglist, toplist)
-#aref = mylist.get_active_reference()
-#mylist.list()
-#print mylist.n_frames
-#
-#print aref.xyz
+if __name__ == "__main__":
+    unittest.main()
--- examples/test_Trajin_Single.py
+++ examples/test_Trajin_Single.py
@@ -1,4 +1,5 @@
 import os
+import unittest
 import numpy as np
 from pycpptraj.Frame import Frame
 from pycpptraj.FrameArray import FrameArray
@@ -10,50 +11,48 @@ from pycpptraj.Topology import Topology
 from pycpptraj.TopologyList import TopologyList
 from pycpptraj.ReferenceFrame import ReferenceFrame
 
-ts = Trajin_Single()
-basetraj = Trajin()
-datadir = os.environ['PYCPPTRAJ_HOME'] + "/examples/data/"
-topname = datadir + "Tc5b.top"
-refname = "./data/Tc5b.nat.crd"
-mdx = "./data/md1_prod.Tc5b.x"
-ts = Trajin_Single()
+class TestTrajinSingle(unittest.TestCase):
 
-top = Topology(topname)
-trajin = """
-"""
-
-ts.load(mdx, ArgList(), top)
-ts.prepare_for_read(True)
-frame = Frame()
-frame.set_frame_v(top, ts.has_vel(), ts.n_repdim)
-frame2 = Frame(frame)
-
-# load reference
-ref = ReferenceFrame()
-ref.load_ref(refname, top)
-ref_frame = ref.frame
-
-# create FrameArray to store Frame
-def get_frame_array():
-    farray = FrameArray()
-    ts.begin_traj(False)
-    for i in range(10):
-        ts.get_next_frame(frame)
-        farray.append(frame)
-        #print frame.rmsd(ref_frame)
-    ts.end_traj()
-    return farray
-
-#print farray.size
-#farray.resize(5)
-#print farray.size
-
-#print "test basetraj"
-#basetraj.load(mdx, ArgList(), top)
-
-#print "Test_Action_Rmsd"
-#armsd = Action_Rmsd()
-#armsd.init(ArgList(), TopologyList(), )
-
+    def test_1(self):
+        global ts
+        ts = Trajin_Single()
+        basetraj = Trajin()
+        datadir = os.environ['PYCPPTRAJ_HOME'] + "/examples/data/"
+        topname = datadir + "Tc5b.top"
+        refname = "./data/Tc5b.nat.crd"
+        mdx = "./data/md1_prod.Tc5b.x"
+        ts = Trajin_Single()
+        
+        top = Topology(topname)
+        trajin = """
+        """
+        
+        ts.load(mdx, ArgList(), top)
+        ts.prepare_for_read(True)
+        frame = Frame()
+        frame.set_frame_v(top, ts.has_vel(), ts.n_repdim)
+        frame2 = Frame(frame)
+        
+        # load reference
+        ref = ReferenceFrame()
+        ref.load_ref(refname, top)
+        ref_frame = ref.frame
+        
+        # create FrameArray to store Frame
+        def get_frame_array():
+            farray = FrameArray()
+            ts.begin_traj(False)
+            for i in range(10):
+                ts.get_next_frame(frame)
+                farray.append(frame)
+                #print frame.rmsd(ref_frame)
+            ts.end_traj()
+            return farray
+        
+        farray = get_frame_array()
+        print farray.size
+        farray.resize(5)
+        print farray.size
+        
 if __name__ == "__main__":
-    pass
+    unittest.main()
