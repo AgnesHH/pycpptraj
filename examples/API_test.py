16dae8a206c9bd8ceb401218ff77f888aaf0edd0
Hai Nguyen <hainm@users.noreply.github.com>
2014-12-26 18:59:29 -0500

    Update API_test.py

--- examples/API_test.py
+++ examples/API_test.py
@@ -7,7 +7,7 @@ state.set_no_progress()
 distance = do_calculation(action=Action_Distance(), input="distance :2@CA :10@CA", state=state)
 
 print distance[:10]
-# make sure to reprodce cpptraj output
+# make sure to reproduce cpptraj output
 
 cppout = np.loadtxt("./data/CAres2_CAres10.Tc5b.dat", skiprows=1).transpose()[1]
 print cppout[:10]
