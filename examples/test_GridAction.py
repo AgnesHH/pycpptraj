5eed5665142b7b8a9559444b8e05206364abc552
hainm <hainm.comp@gmail.com>
2014-12-30 16:55:51 -0500

    update

---
+++ examples/test_GridAction.py
@@ -0,0 +1,11 @@
+import unittest
+from pycpptraj.GridAction import GridAction
+
+class Test(unittest.TestCase):
+    def test_1(self):
+        ga = GridAction()
+        print ga
+
+if __name__ == "__main__":
+    unittest.main()
+
---
+++ pycpptraj/OLD_NAME/GridAction.pyx
@@ -0,0 +1,31 @@
+# distutils: language = c++
+from .cpptraj_dict import GridModeDict, get_key
+
+
+cdef class GridAction:
+    def __cinit__(self):
+        self.thisptr = new _GridAction()
+
+    def __dealloc__(self):
+        del self.thisptr
+
+    #def DataSet_GridFlt * GridInit(self, char *, ArgList arglist, DataSetList dslist, FrameList flist):
+
+    def GridInfo(self, DataSet_GridFlt dset_gf):
+        self.thisptr.GridInfo(dset_gf.thisptr[0])
+
+    def GridSetup(self, Topology top):
+        return self.thisptr.GridSetup(top.thisptr[0])
+
+    def GridFrame(self, Frame frame, AtomMask atm, DataSet_GridFlt dset_gf):
+        self.thisptr.GridFrame(frame.thisptr[0], atm.thisptr[0], dset_gf.thisptr[0])
+
+    def GridMode(self):
+        return get_key(self.thisptr.GridMode(), GridModeDict)
+
+    def CenterMask(self):
+        cdef AtomMask atm = AtomMask()
+        atm.thisptr[0] = self.thisptr.CenterMask()
+
+    def Increment(self):
+        return self.thisptr.Increment()
