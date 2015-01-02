fe3872f469ab460e950ed4859eb0fc367866668e
hainm <hainm.comp@gmail.com>
2015-01-02 15:35:57 -0500

    add test_Action_Strip.py

---
+++ examples/test_Action_Strip.py
@@ -0,0 +1,46 @@
+from pycpptraj.actions.Action_Strip import Action_Strip
+from pycpptraj.actions import Action
+from pycpptraj import Topology, Frame
+from pycpptraj import FrameArray
+from pycpptraj import Trajin_Single
+from pycpptraj import TopologyList, DataSetList, DataFileList
+from pycpptraj import ArgList, FrameList
+
+
+farray = FrameArray(Topology("./data/Tc5b.top"))
+
+print "newtop"
+newtop = farray.top.copy()
+
+trajin = Trajin_Single()
+trajin.load("./data/md1_prod.Tc5b.x", newtop)
+print trajin.max_frames
+farray.get_frames(trajin)
+print farray.size
+
+toplist = TopologyList()
+toplist.add_parm(newtop)
+dslist = DataSetList()
+dflist = DataFileList()
+
+stripact = Action_Strip()
+stripact.read_input(ArgList("strip !@CA"), toplist)
+stripact.process(toplist[0], newtop)
+print "toplist"
+print toplist[0].n_atoms
+print "end toplist"
+
+print 
+farray2 = FrameArray()
+print "before stripping farray2 size = %s" % (farray2.size)
+
+for i in range(10):
+    stripact.do_action_2(farray[i], farray2)
+print "after stripact: farray2"
+print farray2.size
+print farray2[0].coords
+farray2.top.summary()
+farray2.top = newtop.copy()
+farray2.top.summary()
+
+surf = Action_Surf()
