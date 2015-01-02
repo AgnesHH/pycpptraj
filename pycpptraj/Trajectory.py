a1647f22c498f10bc293fa1b64671771f0b21e88
hainm <hainm.comp@gmail.com>
2015-01-02 16:12:20 -0500

    add Trajectory.py (wrapper for Trajin_Single)

---
+++ pycpptraj/Trajectory.py
@@ -0,0 +1,8 @@
+"""This is a thin wrapper of Trajin_Single
+TODO: should combine Trajin_Single and Trajin_Multi
+"""
+from pycpptraj.Trajin_Single import Trajin_Single
+
+class Trajectory(Trajin_Single):
+    def __init__(self):
+        pass
