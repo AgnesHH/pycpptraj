630634bd5d08e5e30679efe497d5f7076105fd39
hnguyen <hnguyen@tabasconew.newtabasco.com>
2015-01-05 16:08:02 -0500

    update

--- examples/test_FrameArray.py
+++ examples/test_FrameArray.py
@@ -43,12 +43,6 @@ class TestFrameArray(unittest.TestCase):
         assert farray.size == N
         # store 10th atom coord of 5th frame for comparison
         # make getting results after printing 3 times
-        print
-        print "10th atom of farray[4]:", farray[4].xyz(10) 
-        print "10th atom of farray[5]:", farray[5].xyz(10) 
-        print "10th atom of farray[6]:", farray[6].xyz(10) 
-        print "10th atom of farray[5]:", farray[5].xyz(10) 
-
         ## framearray size = 10
         print
         print "test FrameArray size"
@@ -60,58 +54,35 @@ class TestFrameArray(unittest.TestCase):
         print 
         frame = farray[5]
         print frame.size
-        print frame.xyz(10)
-        print farray[5].xyz(10)
-        assert frame.xyz(10) == farray[5].xyz(10)
-
         print "do some calculation with 'frame', make sure this will not affect farray[5]"
         print 
         frame += frame
-        print frame.xyz(10) 
-        print farray[5].xyz(10)
-        assert frame.xyz(10) != farray[5].xyz(10)
 
         print "test assignment"
         print 
         farray[5] = frame
-        print farray[5].xyz(10)
-        assert frame.xyz(10) == farray[5].xyz(10)
-        assert farray[5].xyz(10) == frame.xyz(10)
 
         print "test remove Frame from farray"
         print 
         print "10th atom of farray[4 or 5 or 6] before erasing farray[5]"
         print 
-        print farray[4].xyz(10)
-        print farray[5].xyz(10)
-        print farray[6].xyz(10)
         #farray.erase(5)
         del farray[5]
 
         print 
         print "10th atom of farray[4 or 5 or 6] after erasing farray[5]"
         print 
-        print farray[4].xyz(10)
-        print farray[5].xyz(10)
-        print farray[6].xyz(10)
         assert farray.size == N - 1
 
-        assert farray[5].xyz(10) != frame.xyz(10)
-        assert frame.xyz(10) != farray[5].xyz(10)
 
         print "test FrameArray copy"
         farray_cp = farray.copy()
         del farray_cp[5]
         print farray_cp.size
         print farray.size
-        print farray[5].xyz(10)
         assert farray_cp.size != farray.size
         #print farray[5].b_address()
         
-        print farray[5].xyz(10)
-        farray[5].xyz(10)[0] = 1.0
-        print farray[5].xyz(10)
-
         # make sure copy topology too
         assert farray_cp.top.n_atoms == farray.top.n_atoms
 
@@ -124,11 +95,10 @@ class TestFrameArray(unittest.TestCase):
         t.stop()
         print "time for stripping atoms of %s frames is %s" % (N, t.total())
         print farray_cp.top.n_atoms
-        print farray_cp[0].coords
-        print farray_cp[1].coords
+        print farray_cp[0].coords_copy()
 
         arr = load("./data/stripAllButCA.Tc5b.x", (10, 60))
-        np.testing.assert_almost_equal(arr[0], farray_cp[0].coords, decimal=3)
+        np.testing.assert_almost_equal(arr[0], farray_cp[0].coords_copy(), decimal=3)
 
         print "Test printing __str__"
         print farray_cp.size
