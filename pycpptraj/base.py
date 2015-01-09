c74e64bf65d4a8e6af5025f162eb453fe7243e9f
Hai Nguyen <hainm@users.noreply.github.com>
2015-01-08 19:54:49 -0500

    Update base.py

--- pycpptraj/base.py
+++ pycpptraj/base.py
@@ -21,7 +21,7 @@ from pycpptraj.DataFileList import DataFileList
 from pycpptraj.DataSetList import DataSetList
 from pycpptraj.ActionList import ActionList
 from pycpptraj._cast import cast_dataset
-from pycpptraj._iterload import _iterload
+#from pycpptraj._iterload import _iterload
 from pycpptraj import cpptraj_dict
 from pycpptraj.parms.Parm_Amber import Parm_Amber
 
