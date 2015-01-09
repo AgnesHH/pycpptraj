3b1d0280142009af8afc2e6b397480a7da0f9bb6
hainm <hainm.comp@gmail.com>
2015-01-09 13:43:22 -0500

    fix git clone for ./for_travis/install_cython_git.sh

--- for_travis/install_cython_git.sh
+++ for_travis/install_cython_git.sh
@@ -2,7 +2,7 @@
 
 set -ex
 export PYCPPTRAJ_HOME=`pwd`
-git clone https://github.com/cython/cython .
+git clone https://github.com/cython/cython 
 cd cython*
 python ./setup.py install
 cd $PYCPPTRAJ_HOME
