46a669113f4b784b1430ccb6091a7623f8e6f6d6
hainm <hainm.comp@gmail.com>
2015-01-09 13:33:38 -0500

    update _iterload

--- pycpptraj/_iterload.pyx
+++ pycpptraj/_iterload.pyx
@@ -4,6 +4,9 @@ from pycpptraj.Topology cimport Topology
 from pycpptraj.Trajin_Single cimport Trajin_Single
 
 def _iterload(Topology top, string traj, int start=0, chunk=None):
+    """Iterately loading trajectory from file with provided topology file
+    We use cpptraj "Trajin_Single" class to fast reading
+    """
     ts = Trajin_Single()
     ts.load(traj, top)
     if chunk is None:
