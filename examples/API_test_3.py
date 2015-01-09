ae1603dc81dfbd44194fa39204f971c9f8ffb8a6
hainm <hainm.comp@gmail.com>
2015-01-09 17:00:57 -0500

    add API test

--- examples/API_test_3.py
+++ examples/API_test_3.py
@@ -7,47 +7,26 @@ from pycpptraj.actions.Action_Distance import Action_Distance
 from test_API.TestAPI2 import create_state, do_calculation
 
 state = create_state(top="./data/Tc5b.top", trajin="./data/md1_prod.Tc5b.x", ref=None)
+state.add_trajout("./out.x")
 state.set_no_progress()
-#output = do_calculation(action=Action_Strip(), command="strip !@CA", state=state)
-output = do_calculation(action=Action_Distance(), command="distance :2@CA :10@CA", state=state)
-
-dlist = state.datasetlist
+output = do_calculation(action=Action_Strip(), command="strip !@CA", state=state)
+trajlist0 = output.get_trajinlist()
 trajlist = state.get_trajinlist()
-print trajlist
-print dir(trajlist)
-print dir(trajlist[0])
-trajin = trajlist[0]
-print trajin.current_frame
-print trajin.start
-print trajin.offset
-print trajin.top.n_atoms
-trajin.top = state.toplist[0]
-print trajin.top.n_atoms
-trajin.print_info_line()
-print dlist[0]
 
-frame = Frame(trajin.top.n_atoms)
-print frame
-#trajin.begin_traj()
-#trajin.get_next_frame(frame)
-#trajin.end_traj()
+print trajlist0, trajlist
+traj = trajlist[0]
+print traj.size
+print traj.is_empty()
+print traj.top.n_atoms
+traj.top = Topology("./data/Tc5b.top")
+print traj.top.n_atoms
+print traj.current_frame
+print traj[0].n_atoms
 
-with trajin:
-    trajin.get_next_frame(frame)
-
-print np.asarray(frame[:10])
-print frame.size
-print frame.n_atoms
-
-print "traji.max_frames = ", trajin.max_frames
-i = 0
-for traj in trajin:
-    i += 1
-print i
-
-#gen = trajin.__iter__()
-#frame2 = gen.next()
-#print frame2
-#print np.asarray(frame2[:10])
-#print frame2.size
-#print frame2.n_atoms
+dflist = state.datafilelist
+dslist = state.datasetlist
+print dir(dflist)
+print dir(dslist)
+print dslist.is_empty()
+print dslist.size
+print dir(state)
--- pycpptraj/actions/__init__.py
+++ pycpptraj/actions/__init__.py
@@ -2,16 +2,17 @@
 from __future__ import absolute_import
 from glob import glob
 
-# get a list of Actions
-actionlist = [fullname.split(".")[0] for fullname in glob("Action*.pxd")]
-excludedList = ['Action_MRT', ]
-
-for ext in excludedList:
-    actionlist.remove(ext)
-
-__all__ = actionlist
-
-for action in actionlist:
-    #full_dir_action = ".".join(['pycpptraj', 'action', action, action])
-    full_dir_action = ".".join(['pycpptraj', 'action', action])
-    __import__(full_dir_action)
+# FIXME
+## get a list of Actions
+#actionlist = [fullname.split(".")[0] for fullname in glob("Action*.pxd")]
+#excludedList = ['Action_MRT', ]
+#
+#for ext in excludedList:
+#    actionlist.remove(ext)
+#
+#__all__ = actionlist
+#
+#for action in actionlist:
+#    #full_dir_action = ".".join(['pycpptraj', 'action', action, action])
+#    full_dir_action = ".".join(['pycpptraj', 'action', action])
+#    __import__(full_dir_action)
