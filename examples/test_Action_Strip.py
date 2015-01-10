8c620b46ccb39850db4aac2ae3cecca38b53bb4c
hainm <hainm.comp@gmail.com>
2015-01-10 03:29:41 -0500

    update

--- examples/test_Action_Strip.py
+++ examples/test_Action_Strip.py
@@ -23,26 +23,3 @@ stripact.read_input(ArgList("strip !@CA"), toplist)
 stripact.process(toplist[0], newtop=newtop)
 stripact.do_action(0, farray[0])
 print farray[0].n_atoms
-#print "toplist"
-#print "toplist[0].n_atom ", toplist[0].n_atoms
-#print "end toplist"
-#print "newtop.n_atoms", newtop.n_atoms
-#newtop.summary()
-
-print farray[0].size
-strip(farray, "!@CA")
-print farray.top.n_atoms
-print "After stripping, farray2.top"
-farray.top.summary()
-
-newtop2 = strip(farray2.top, "!@CA")
-print "After stripping, newtop2"
-newtop2.summary()
-
-print "test Frame.strip_atoms"
-for i in range(farray2.size):
-    # this method is too slow vs cpptraj
-    tmp = farray2[i]
-    tmp.strip_atoms(farray2.top.copy(), "!@CA")
-    farray2[i] = tmp
-
