0771bcf7b4e848f7fe5366705eb1cede4696a941
hainm <hainm.comp@gmail.com>
2015-01-10 20:41:51 -0500

    update

--- pycpptraj/cast_dataset.pyx
+++ pycpptraj/cast_dataset.pyx
@@ -5,14 +5,14 @@ from pycpptraj.datasets.DataSet_double cimport DataSet_double, _DataSet_double
 from pycpptraj.datasets.DataSet_MatrixDbl cimport DataSet_MatrixDbl, _DataSet_MatrixDbl
 from pycpptraj.datasets.DataSet cimport DataSet, _DataSet
 
-def cast_dataset(DataSet dset, dtype='dataset_double'):
+def cast_dataset(DataSet dset, dtype='general'):
     """create memoryview for DataSet instance. 
     DataSet instace is taken from DatatSetList
     Parameters
     ---------
     dset : DataSet instance
     dtype : str (default dtype=None)
-        {'dataset_double', 'dataset_matrixdbl', 'dataset_1d', 'dataset_2d'}
+        {'general', 'matrix', '1D', '2D'}
     """
     # TODO : rename data set
     cdef DataSet_1D newset1D
@@ -20,19 +20,19 @@ def cast_dataset(DataSet dset, dtype='dataset_double'):
     cdef DataSet_double newset_double
     cdef DataSet_MatrixDbl newset_MatrixDbl
 
-    if dtype == 'dataset_1d':
+    if dtype == '1D':
         newset1D = DataSet_1D()
         # need to recast baseptr0
         newset1D.baseptr0 =  dset.baseptr0
         # need to recast baseptr_1
         newset1D._recast_pointers(0)
         return newset1D
-    if dtype == 'dataset_2d':
+    if dtype == '2D':
         newset2D = DataSet_2D()
         newset2D.baseptr0 = <_DataSet*> dset.baseptr0
         newset2D.baseptr_1 = <_DataSet_2D*> dset.baseptr0
         return newset2D
-    elif dtype == 'dataset_double':
+    elif dtype == 'general':
         newset_double = DataSet_double()
         # since we introduce memory view, we let cpptraj free memory
         newset_double.py_free_mem = False
@@ -41,7 +41,7 @@ def cast_dataset(DataSet dset, dtype='dataset_double'):
         newset_double.baseptr_1 = <_DataSet_1D*> dset.baseptr0
         newset_double.thisptr = <_DataSet_double*> dset.baseptr0
         return newset_double
-    elif dtype == 'dataset_matrixdbl':
+    elif dtype == 'matrix':
         newset_matrixdbl = DataSet_MatrixDbl()
         # since we introduce memory view, we let cpptraj free memory
         newset_matrixdbl.py_free_mem = False
