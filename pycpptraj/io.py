d53b97ca18c5e6fb9a65a00f467bb66a0e91c666
hainm <hainm.comp@gmail.com>
2015-01-13 15:32:05 -0500

    update pycpptraj/io.py

--- pycpptraj/io.py
+++ pycpptraj/io.py
@@ -8,14 +8,15 @@ from pycpptraj.Topology import Topology
 
 __all__ = ['load', 'iterload', 'writetraj', 'readparm', 'writeparm']
 
-def iterload(top=None, fname=None, start=0, chunk=None):
-    '''Iterately return Frame instance'''
+def iterload(fname=None, top=Topology(), start=0, chunk=None):
+    """iterload(fname=None, top=Topology(), start=0, chunk=None)"""
     if not isinstance(top, Topology):
         # string
         top = Topology(top)
     return _iterload(top, fname, start, chunk)
 
-def load(top=None, fname=None, readonly=True):
+def load(fname=None, top=Topology(), readonly=True):
+    """load(fname=None, top=Topology(), readonly=True)"""
     if not isinstance(top, Topology):
         # string
         top = Topology(top)
@@ -27,25 +28,28 @@ def load(top=None, fname=None, readonly=True):
     return ts
 
 def writetraj(fname="", traj=Trajectory(), top=Topology(), 
+              fmt='AMBERTRAJ', indices=None):
+    """writetraj(fname="", traj=Trajectory(), top=Topology(), 
               ftm='AMBERTRAJ', indices=None):
-    """write Frame or FrameArray"""
-    trajout = Trajout()
-    trajout.openfile(fname=fname, top=top, fmt=ftm)
-    if indices:
-        raise NotImplementedError("not yet supportted")
-    if isinstance(traj, Frame):
-        if indices is not None:
-            raise ValueError("indices does not work with single Frame")
-        trajout.writeframe(0, top, traj)
-    elif isinstance(traj, FrameArray) or isinstance(traj, Trajin_Single):
-        # assume that 0w
-        for idx, frame in enumerate(traj):
-            trajout.writeframe(idx, top, frame)
-    trajout.closefile()
+    """
+    with Trajout(fname=fname, top=top, fmt=fmt) as trajout:
+        if isinstance(traj, Frame):
+            if indices is not None:
+                raise ValueError("indices does not work with single Frame")
+            trajout.writeframe(0, traj, top)
+        elif isinstance(traj, FrameArray) or isinstance(traj, Trajin_Single):
+            # assume
+            if not indices:
+                # write all traj
+                for idx, frame in enumerate(traj):
+                    trajout.writeframe(idx, frame, top)
+            else:
+                for idx in indices:
+                    trajout.writeframe(idx, traj[idx], top)
 
-def writeparm(fname=None, top=None, fmt=''):
+def writeparm(fname=None, top=None, fmt='AMBERPARM'):
     # TODO : add *args
-    from pycpptraj.ParmFile import ParmFile
+    from pycpptraj.parms.ParmFile import ParmFile
     parm = ParmFile()
     parm.writeparm(fname=fname, top=top, fmt=fmt)
 
