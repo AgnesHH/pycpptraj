270c0debeab3772783d2db0d45c2fd0fc824049b
hainm <hainm.comp@gmail.com>
2015-01-13 18:29:34 -0500

    update

--- tests/TestListTravis.sh
+++ tests/TestListTravis.sh
@@ -1,8 +1,11 @@
 #!/bin/sh
 
+python ./API_test.py
 python ./test_base.py
 python ./test_Trajectory.py
 python ./test_Frame.py
 python ./test_Topology.py
 python ./test_FrameArray2.py
 python ./test_FrameArray.py
+python ./test_io.py
+python ./test_ParmFile.py
