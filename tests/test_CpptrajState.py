0ae47c369aa4358af60bf5b66ce98ed96f5abad9
hainm <hainm.comp@gmail.com>
2015-01-13 23:33:30 -0500

    update tests/test_CpptrajState.py

--- tests/test_CpptrajState.py
+++ tests/test_CpptrajState.py
@@ -1,11 +1,12 @@
 import os
 from pycpptraj.Timer import Timer
+from pycpptraj.base import FrameArray
 from pycpptraj.actions.Action_Box import Action_Box
-from pycpptraj.DataSet_1D import DataSet_1D
+from pycpptraj.datasets.DataSet_1D import DataSet_1D
 from pycpptraj.DataFileList import DataFileList
-from pycpptraj._cast import cast_dataset
+from pycpptraj.cast_dataset import cast_dataset
 from pycpptraj.DataSetList import DataSetList
-from pycpptraj.DataSet import DataSet
+from pycpptraj.datasets.DataSet import DataSet
 from pycpptraj.actions.Action_Dihedral import Action_Dihedral
 from pycpptraj.actions.Action_Rmsd import Action_Rmsd
 from pycpptraj.actions.Action_Distance import Action_Distance
@@ -16,13 +17,13 @@ from pycpptraj.ArgList import ArgList
 from pycpptraj.FileName import FileName
 from pycpptraj.CpptrajState import CpptrajState
 from pycpptraj.CpptrajFile import CpptrajFile
-from pycpptraj.Trajin import Trajin
+from pycpptraj.trajs.Trajin import Trajin
 from pycpptraj.TrajinList import TrajinList
 from pycpptraj.ReferenceFrame import ReferenceFrame
 from pycpptraj.FrameList import FrameList
 
 # setup filenames
-datadir = os.environ['PYCPPTRAJ_HOME'] + "/examples/data/"
+datadir = "./data/"
 topname = datadir + "Tc5b.top"
 trajoutname = datadir + "test.x"
 refname = "./data/Tc5b.nat.crd"
@@ -75,52 +76,40 @@ class TestCpptrajState(unittest.TestCase):
         dslist = state2.datasetlist
         d0 = dslist[0]
         print "d0 is empty? ", d0.is_empty()
-        d1 = cast_dataset(d0, dtype="dataset_double")
+        d1 = cast_dataset(d0, dtype="general")
         print "d1 is empty? ", d1.is_empty()
         print d1.data[0:10]
-        #state2.get_trajinlist().list()
-
-        ## get_trajinlist() will return an instance copy of TrajinList
-        #state2.get_trajinlist().add_trajin("./data/md1_prod.Tc5b.x", ArgList(), toplist)
-
-        #print "TEST: state2.trajinlist.list()"
-        #state2.get_trajinlist().list()
-        #print "TEST: state2.trajinlist.list() using state"
-        #for i in range(10):
-        #    state2.add_trajin("./data/md1_prod.Tc5b.x")
-        #state2.get_trajinlist().list()
 
 
-#    def test_action(self):
-#        distaction = Action_Distance()
-#        boxaction = Action_Box()
-#        boxaction.help()
-#        toplist = TopologyList()
-#        framelist = FrameList()
-#        dsetlist = DataSetList()
-#        dflist = DataFileList()
-#
-#        # add stuff
-#        toplist.add_parm("./data/Tc5b.top")
-#        framelist.add_reference(ArgList("./data/Tc5b.nat.crd"), toplist)
-#        distaction.read_input(ArgList(":2@CA :10@CA"), toplist, framelist, dsetlist, dflist, 0)
-#        boxaction.read_input(ArgList("x 1000. y 1000. alpha 500."), toplist, framelist, dsetlist, dflist, 0)
-#        distaction.process(toplist[0])
-#        from test_Trajin_Single import get_frame_array
-#        farray = get_frame_array()
-#        #print farray
-#        idx = 0
-#        print "Do action:"
-#        print "============================="
-#        distaction.do_action(idx, farray[idx])
-#        frame0 = farray[idx]
-#        # update Frame instance with new Box info
-#        boxaction.do_action(idx, frame0)
-#        boxnew = frame0.b_address()
-#        #print boxnew.boxarr
-#        boxaction.print_output()
-#        distaction.print_output()
-#        # how to get output?
+    def test_action(self):
+        distaction = Action_Distance()
+        boxaction = Action_Box()
+        boxaction.help()
+        toplist = TopologyList()
+        framelist = FrameList()
+        dsetlist = DataSetList()
+        dflist = DataFileList()
+
+        # add stuff
+        toplist.add_parm("./data/Tc5b.top")
+        framelist.add_reference(ArgList("./data/Tc5b.nat.crd"), toplist)
+        distaction.read_input(ArgList(":2@CA :10@CA"), toplist, framelist, dsetlist, dflist, 0)
+        boxaction.read_input(ArgList("x 1000. y 1000. alpha 500."), toplist, framelist, dsetlist, dflist, 0)
+        distaction.process(toplist[0])
+
+        idx = 0
+        farray = FrameArray("./data/Tc5b.nat.crd", "./data/Tc5b.top")
+        distaction.do_action(0, farray[idx])
+        frame0 = farray[idx]
+        # update Frame instance with new Box info
+        boxaction.do_action(idx, frame0)
+        boxnew = frame0.get_box()
+
+        print "box = ", boxnew.boxarr
+        print boxnew
+        boxaction.print_output()
+        distaction.print_output()
+        # how to get output?
 #
 if __name__ == "__main__":
     unittest.main()
