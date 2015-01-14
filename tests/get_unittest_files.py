113671932d33899599915bc93ca86ab3e06f5a90
hainm <hainm.comp@gmail.com>
2015-01-14 17:26:15 -0500

    update tests/get_unittest_files.py

--- tests/get_unittest_files.py
+++ tests/get_unittest_files.py
@@ -1,6 +1,8 @@
 from glob import glob
 
-for pyfile in glob("test_*.py"):
+
+testlist = glob("test_*.py") + ['API_test.py', 'API_test_3.py',]
+for pyfile in testlist:
     with open(pyfile, 'r') as fh:
         txt = fh.read()
         if "import unittest" in txt:
