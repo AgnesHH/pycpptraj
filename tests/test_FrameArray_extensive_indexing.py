6ba574883466a09b2a3896011056672121eceb9b
hainm <hainm.comp@gmail.com>
2015-01-14 14:05:23 -0500

    update tests/test_FrameArray_extensive_indexing.py

--- tests/test_FrameArray_extensive_indexing.py
+++ tests/test_FrameArray_extensive_indexing.py
@@ -33,18 +33,56 @@ FARRAY = ts[:FRAMENUM]
 
 class TestFrameArray(unittest.TestCase):
 
-    def test_reverse_0(self):
-        farray = FARRAY.copy()
-        N = farray.size
-        #farray_sub = farray[:-1:-3]
-        print "SPECIALLLLLLLLLLLLLLLLLLLLLLL"
-        print N
-        farray_sub = farray[:-3]
-        assert farray_sub.size + 3 == N
-        print farray_sub.size
-        print "SPECIALLLLLLLLLLLLLLLLLLLLLLL"
-        print farray_sub
-        #assert farray[N-1].has_same_coords(farray_sub[0])
+    def test_len(self):
+        N = 10
+        farray = FARRAY[:N].copy()
+        assert farray.size == N
+        old_coords_5_10 = farray[5].coords[:10]
+        assert farray[:3].size == 3
+        assert farray[1:3].size == 2
+        assert farray[3:1].size == 0
+        assert farray[3:1:-1].size == 2
+        assert farray[-1:-3].size == 0
+        assert farray[-1:-3:-1].size == 2
+        assert farray[-1].same_coords_as(farray[N-1]) == True
+
+        #assert farray[5:1:-1][0].same_coords_as(farray[5]) == True
+        # segment fault if using below expression
+        #print farray[5:1:-1][0].coords[:10]
+
+        # need to create a temp farray
+        subfarray = farray[5:1:-1]
+        print subfarray
+        assert subfarray[0].same_coords_as(farray[5]) == True
+        assert old_coords_5_10 == farray[5].coords[:10]
+        print subfarray[0].coords[:10]
+        print farray[5].coords[:10]
+
+    def test_len_Trajin_Single(self):
+        # create alias of `ts` (Trajin_Single instance  created above)
+        farray = ts
+        N = ts.size
+        assert farray.size == N
+        old_coords_5_10 = farray[5].coords[:10]
+        assert farray[:3].size == 3
+        assert farray[1:3].size == 2
+        assert farray[3:1].size == 0
+        assert farray[3:1:-1].size == 2
+        assert farray[-1:-3].size == 0
+        assert farray[-1:-3:-1].size == 2
+        assert farray[-1].same_coords_as(farray[N-1]) == True
+
+        #assert farray[5:1:-1][0].same_coords_as(farray[5]) == True
+        # segment fault if using below expression
+        #print farray[5:1:-1][0].coords[:10]
+
+        # need to create a temp farray
+        subfarray = farray[5:1:-1]
+        print subfarray
+        assert subfarray[0].same_coords_as(farray[5]) == True
+        assert old_coords_5_10 == farray[5].coords[:10]
+        print subfarray[0].coords[:10]
+        print farray[5].coords[:10]
 
 if __name__ == "__main__":
     unittest.main()
