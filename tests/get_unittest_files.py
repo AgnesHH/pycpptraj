56625aefaaa0737aa27f9150de17c70ff8229854
hainm <hainm.comp@gmail.com>
2015-01-17 03:47:38 -0500

    update tests/get_unittest_files.py

--- tests/get_unittest_files.py
+++ tests/get_unittest_files.py
@@ -7,7 +7,8 @@ for pyfile in testlist:
     with open(pyfile, 'r') as fh:
         txt = fh.read()
         if "import unittest" in txt:
-            lines.append("python ./%s \n") % pyfile
+            line = "python ./%s \n" % pyfile
+            lines.append(line)
 
 with open("./TestListTravis.sh", 'w') as fh:
     fh.writelines(lines)
