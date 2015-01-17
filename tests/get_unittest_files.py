3ff4251741b1465920fba51b4357a90bcf86f1b3
hainm <hainm.comp@gmail.com>
2015-01-17 03:36:25 -0500

    update tests/get_unittest_files.py

--- tests/get_unittest_files.py
+++ tests/get_unittest_files.py
@@ -1,9 +1,13 @@
 from glob import glob
 
 
+lines = []
 testlist = glob("test_*.py") + ['API_test.py', 'API_test_3.py',]
 for pyfile in testlist:
     with open(pyfile, 'r') as fh:
         txt = fh.read()
         if "import unittest" in txt:
-            print "python ./%s" % pyfile
+            lines.append("python ./%s \n") % pyfile
+
+with open("./TestListTravis.sh", 'w') as fh:
+    fh.writelines(lines)
