26754a6f93a2c0f91ce61833e0fd5210179230b1
hainm <hainm.comp@gmail.com>
2015-01-17 01:41:19 -0500

    update

--- examples/create_new_parm.py
+++ examples/create_new_parm.py
@@ -1,1 +1,9 @@
-pass
+from pycpptraj import io as mdio
+
+top = mdio.readparm("./data/Tc5b.top")
+
+# strip all atoms but CA
+top.strip_atoms("!@CA")
+
+# save new AMBER parm to file
+mdio.writeparm("_Tcb5.onlyCA.top", top)
