b36c08dece00b7353d6ce6456cf9f4c1bd187c28
hainm <hainm.comp@gmail.com>
2015-01-17 01:32:36 -0500

    update tests/test_io.py

--- tests/test_io.py
+++ tests/test_io.py
@@ -7,7 +7,7 @@ class TestPyCpptrajIO(unittest.TestCase):
         top = mdio.load("./data/Tc5b.top")
         assert isinstance(top, Topology) == True
 
-        traj = mdio.load(fname="./data/md1_prod.Tc5b.x", top="./data/Tc5b.top")
+        traj = mdio.load(filename="./data/md1_prod.Tc5b.x", top="./data/Tc5b.top")
 
         # TODO : add more classes here
         is_traj = (isinstance(traj, Trajin_Single) or isinstance(traj, FrameArray))
@@ -20,19 +20,19 @@ class TestPyCpptrajIO(unittest.TestCase):
         assert top.n_atoms == newtop.n_atoms
 
     def test_load_and_save(self):
-        traj = mdio.load(fname="./data/md1_prod.Tc5b.x", top="./data/Tc5b.top")
-        trajiter = mdio.iterload(fname="./data/md1_prod.Tc5b.x", top="./data/Tc5b.top", chunk=100)
+        traj = mdio.load(filename="./data/md1_prod.Tc5b.x", top="./data/Tc5b.top")
+        trajiter = mdio.iterload(filename="./data/md1_prod.Tc5b.x", top="./data/Tc5b.top", chunk=100)
         print trajiter
 
         indices = range(20, 30, 5) + [103, 78, 90, 82]
-        mdio.writetraj(fname="./output/test_io_saved.x", 
+        mdio.writetraj(filename="./output/test_io_saved.x", 
                        traj=traj, 
                        top="./data/Tc5b.top",
                        fmt='AMBERTRAJ', 
                        indices=indices)
 
         # check frames
-        traj = mdio.load(fname="./output/test_io_saved.x", top="./data/Tc5b.top")
+        traj = mdio.load(filename="./output/test_io_saved.x", top="./data/Tc5b.top")
         assert traj.size == len(indices) 
 
 if __name__ == "__main__":
