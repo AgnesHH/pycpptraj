9af5ea82d32e67c0f2c421edaa284ad511b8af99
hainm <hainm.comp@gmail.com>
2015-01-17 03:51:26 -0500

    update tests/RunAllAndFindFailure.py

--- tests/RunAllAndFindFailure.py
+++ tests/RunAllAndFindFailure.py
@@ -1,2 +1,11 @@
 from subprocess import call
-call(['path_to_tool','-option1','option2'], stdout = file_out, stderr = log_file)
+
+with open("log", 'w') as log_file, open("output.txt", 'w') as file_out:
+    call(['python', './get_unittest_files.py'])
+    call(['sh','.//TestListTravis.sh'], stdout = file_out, stderr = log_file)
+
+    i_fails = 0
+    for line in log_file.readlines():
+        if "File" in line:
+            i_fails += 1
+            print line
