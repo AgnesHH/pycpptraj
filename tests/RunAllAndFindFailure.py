70ba01e46d87138a3ebb853ff1310d3d6bfba471
hainm <hainm.comp@gmail.com>
2015-01-17 03:03:07 -0500

    update tests/RunAllAndFindFailure.py

--- tests/RunAllAndFindFailure.py
+++ tests/RunAllAndFindFailure.py
@@ -4,7 +4,8 @@ from subprocess import Popen
 
 import commands
 txt = commands.getstatusoutput('sh ./TestListTravis.sh')
+
 i = 0
-if 'ERROR' in txt:
+if 'ERROR' in txt or 'error' in txt or 'Error' in txt:
     i += 1
 print "%s ERRORs" % i
