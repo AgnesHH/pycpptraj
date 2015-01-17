c89c1a3cb92c8ca55d286e6c4e4f43fd8d0a94e0
hainm <hainm.comp@gmail.com>
2015-01-17 01:32:29 -0500

    update tests/test_ReferenceFrame.py

--- tests/test_ReferenceFrame.py
+++ tests/test_ReferenceFrame.py
@@ -10,16 +10,16 @@ from pycpptraj.ArgList import ArgList
 
 datadir = os.environ['PYCPPTRAJ_HOME'] + "/examples/data/"
 topname = datadir + "Tc5b.top"
-fname = "./data/md1_prod.Tc5b.x"
+filename = "./data/md1_prod.Tc5b.x"
 
 top = Topology(topname)
 #top.summary()
 ref = ReferenceFrame()
-#ref.load_ref(filename=fname, top=top, debug=0, arglist=ArgList(), mask="@CA")
-ref.load_ref(fname, top, arglist=ArgList(), mask="@CA")
+#ref.load_ref(filename=filename, top=top, debug=0, arglist=ArgList(), mask="@CA")
+ref.load_ref(filename, top, arglist=ArgList(), mask="@CA")
 ref.top.summary()
 print ref.frame
 name = ref.frame_name()
-assert name.full_name == fname
+assert name.full_name == filename
 #assert name.ext == '.crd'
 ref.info()
