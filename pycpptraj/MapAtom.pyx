44fc50a0fd4ba830519fdf6afa05ccba3286ad0d
hainm <hainm.comp@gmail.com>
2015-01-19 12:28:32 -0500

    update pycpptraj/MapAtom.pyx

--- pycpptraj/MapAtom.pyx
+++ pycpptraj/MapAtom.pyx
@@ -7,18 +7,21 @@ cdef class MapAtom:
         cdef MapAtom map_atom
 
         if not args:
-            self.thisptr = new _MapAtom()
+            self.thisptr_ma = new _MapAtom()
         elif len(args) == 1:
             if isinstance(args[0], Atom):
                 atom = args[0]
-                self.thisptr = new _MapAtom(atom.thisptr[0])
+                self.thisptr_ma = new _MapAtom(atom.thisptr_ma[0])
             elif isinstance(args[0], MapAtom):
                 map_atom = args[0]
-                self.thisptr = new _MapAtom(map_atom.thisptr[0])
+                self.thisptr_ma = new _MapAtom(map_atom.thisptr_ma[0])
             else:
                 raise ValueError()
         else:
             raise ValueError()
+
+        # recast pointers since MapAtom inherits from Atom
+        self.thisptr = <_Atom*> self.thisptr_ma
 
     def copy(self):
         """Return a copy of instance
@@ -28,59 +31,59 @@ cdef class MapAtom:
         >>>print ma1 == ma2
         """
         cdef MapAtom matom = MapAtom()
-        matom.thisptr[0] = self.thisptr[0]
+        matom.thisptr_ma[0] = self.thisptr_ma[0]
         return matom
 
     def is_chiral(self):
-        return self.thisptr.IsChiral()
+        return self.thisptr_ma.IsChiral()
 
     def bound_to_chiral(self):
-        return self.thisptr.BoundToChiral()
+        return self.thisptr_ma.BoundToChiral()
 
     def is_mapped(self):
-        return self.thisptr.IsMapped()
+        return self.thisptr_ma.IsMapped()
 
     def complete(self):
-        return self.thisptr.Complete()
+        return self.thisptr_ma.Complete()
 
     def is_unique(self):
-        return self.thisptr.IsUnique()
+        return self.thisptr_ma.IsUnique()
 
     def unique(self):
-        return self.thisptr.Unique()
+        return self.thisptr_ma.Unique()
 
     def n_duplicated(self):
-        return self.thisptr.Nduplicated()
+        return self.thisptr_ma.Nduplicated()
 
     def char_name(self):
-        return self.thisptr.CharName()
+        return self.thisptr_ma.CharName()
 
     def is_duplicated(self):
-        self.thisptr.IsDuplicated()
+        self.thisptr_ma.IsDuplicated()
 
     def set_mapped(self):
-        self.thisptr.SetMapped()
+        self.thisptr_ma.SetMapped()
 
     def set_complete(self):
-        self.thisptr.SetComplete()
+        self.thisptr_ma.SetComplete()
 
     def set_chiral(self):
-        self.thisptr.SetChiral()
+        self.thisptr_ma.SetChiral()
 
     def set_bound_to_chiral(self):
-        self.thisptr.SetBoundToChiral()
+        self.thisptr_ma.SetBoundToChiral()
 
-    def set_atom_i_d(self, string s):
-        self.thisptr.SetAtomID(s)
+    def set_atom_id(self, string s):
+        self.thisptr_ma.SetAtomID(s)
 
     def set_unique(self, string s):
-        self.thisptr.SetUnique(s)
+        self.thisptr_ma.SetUnique(s)
 
     def set_not_mapped(self):
-        self.thisptr.SetNotMapped()
+        self.thisptr_ma.SetNotMapped()
 
     def set_not_complete(self):
-        self.thisptr.SetNotComplete()
+        self.thisptr_ma.SetNotComplete()
 
     def set_not_chiral(self):
-        self.thisptr.SetNotChiral()
+        self.thisptr_ma.SetNotChiral()
