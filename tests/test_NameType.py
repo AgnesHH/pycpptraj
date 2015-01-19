08a0d214b80d4e0a52d3d2d1a27d39ff160d9be9
hainm <hainm.comp@gmail.com>
2015-01-19 12:42:35 -0500

    update tests/test_NameType.py

--- tests/test_NameType.py
+++ tests/test_NameType.py
@@ -1,39 +1,12 @@
+import unittest
+from pycpptraj.base import *
+from pycpptraj import io as mdio
 from pycpptraj.NameType import NameType
-# TODO: write testing 
 
-# test create instance
-nt1 = NameType("test11111")
-nt2 = NameType("test11111")
-nt3 = NameType(nt1)
+class TestNameType(unittest.TestCase):
+    def test_0(self):
+        nt = NameType()
+        print dir(nt)
 
-print "test Match"
-print nt1.Match(nt2)
-print nt2.Match(nt3)
-print
-
-print "test != operator"
-print nt1 != nt2
-print nt1 != nt3
-
-nt4 = NameType("t")
-print nt1 != nt4
-print nt1.Match(nt4)
-print
-
-# test indexing
-print "test indexing"
-print nt1[3]
-
-print "test == operator"
-print nt4 == "t"
-print nt1 == 'test1'
-print
-
-# 
-print "test Truncated"
-nt1.Truncated()
-print nt1 == nt4
-print "test ToBuffer"
-# s = ''
-# nt1.ToBuffer(s)
-# print s
+if __name__ == "__main__":
+    unittest.main()
