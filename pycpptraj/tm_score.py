9360fe595c6efd86881a7646204f6c0d897f0490
hainm <hainm.comp@gmail.com>
2015-01-19 15:28:45 -0500

    update pycpptraj/tm_score.py

--- pycpptraj/tm_score.py
+++ pycpptraj/tm_score.py
@@ -1,14 +1,11 @@
-# from CSB package
+# adapted from CSB package
 # turn-off numpy.sum, numpy.power
 # use builtin `sum`
 # TODO : add more acknowledgement
+from pycpptraj.DistRoutines import distance_frames
 
 def _tm_d0(Lmin):
-    
-    #from numpy import power
-     
     if Lmin > 15:
-        #d0 = 1.24 * power(Lmin - 15.0, 1.0 / 3.0) - 1.8
         d0 = 1.24 * (Lmin - 15.0)**(1.0 / 3.0) - 1.8
     else:
         d0 = 0.5
@@ -16,26 +13,24 @@ def _tm_d0(Lmin):
 
 def tm_score(x, y, L=None, d0=None):
     """
-    Evaluate the TM-score of two conformations as they are (no fitting is done).
+    Evaluate the TM-score of two Frames as they are (no fitting is done).
+    Return : float
     
-    @param x: 3 x n input array
-    @type x: numpy array
-    @param y: 3 x n input array
-    @type y: numpy array
-    @param L: length for normalization (default: C{len(x)})
-    @type L: int
-    @param d0: d0 in Angstroms (default: calculate from C{L})
-    @type d0: float
-    
-    @return: computed TM-score
-    @rtype: float
+    Parameters:
+    ----------
+    x : Frame instance
+        Frame 1
+    y : Frame instance 
+        Frame 2
+    L: length for normalization (default: ?)
+    d0: d0 in Angstroms (default: calculate from C{L})
     """
-    #from numpy import sum
+    import numpy as np
 
     if not L:
-        L = len(x)
+        L = x.n_atoms
     if not d0:
         d0 = _tm_d0(L)
-    d = distance(x, y)
-
+    # convert Python array to numpy array 
+    d = np.array(distance_frames(x, y))
     return sum(1 / (1 + (d / d0) ** 2)) / L
