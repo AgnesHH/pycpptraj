2a24985ffb0b184390deb52b4878b42ea74a500d
hainm <hainm.comp@gmail.com>
2015-01-19 13:10:17 -0500

    update examples/calc_pairwise_distance.py

--- examples/calc_pairwise_distance.py
+++ examples/calc_pairwise_distance.py
@@ -13,6 +13,9 @@ def calc_pairwise_distance():
     print distance(frame0.coords[0:3], frame0.coords[96:99])
     natoms = frame0.n_atoms
     arr = np.empty((natoms, natoms))
+
+    # this is the demo. For large system, use cpptraj or Cython
+    # for nested loop (which is quite expensive in Python)
     for i in range(frame0.n_atoms):
         for j in range(frame0.n_atoms):
             arr[i, j] = distance(frame0.atom_xyz(i), frame0.atom_xyz(j))
