e28dea97717436d4a08a1b4c5bea33c8761b2ccb
hainm <hainm.comp@gmail.com>
2015-01-19 18:25:35 -0500

    update tests/test_TMscore.py

--- tests/test_TMscore.py
+++ tests/test_TMscore.py
@@ -6,7 +6,14 @@ from pycpptraj.tm_score import tm_score
 class Test(unittest.TestCase):
     def test_0(self):
         traj = mdio.load("./data/md1_prod.Tc5b.x", "./data/Tc5b.top")
-        print tm_score(traj[0], traj[1])
+        tm_0 = tm_score(traj[0], traj[1])
+        print tm_0
+        print tm_score(traj[0], traj[0])
+        print tm_score(traj[0], traj[1], mask="!@CA", top=traj.top)
+        print tm_score(traj[0], traj[1], mask1="!:2-10@CA", top1=traj.top,
+                                         mask2="!:2-10@CA", top2=traj.top)
+
+        #assert tm_0 == ?
 
 if __name__ == "__main__":
     unittest.main()
