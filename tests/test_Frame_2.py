02cd54c12264bf1cddcbb3b1897da46691436e8c
hainm <hainm.comp@gmail.com>
2015-01-20 18:00:59 -0500

    update

---
+++ tests/test_Frame_2.py
@@ -0,0 +1,47 @@
+import unittest
+from array import array
+import numpy as np
+from numpy.testing import assert_almost_equal
+from pycpptraj.Frame import Frame
+from pycpptraj.base import *
+from pycpptraj import io as mdio
+from pycpptraj.decorators import no_test
+
+SMALL = 1E-6
+
+N_ATOMS = 10
+FRAME = Frame(N_ATOMS)
+arr = np.arange(3 * N_ATOMS)
+FRAME.set_from_crd(arr)
+FRAME_orig = FRAME.copy()
+
+class TestFrame(unittest.TestCase):
+    ##@no_test
+    def test_fit(self):
+        traj = mdio.load("./data/md1_prod.Tc5b.x", "./data/Tc5b.top")
+        trajnew = mdio.load("./data/md1_prod.fit_to_first.Tc5b.x", "./data/Tc5b.top")
+
+        # make sure 0-th frame does not change
+        frame0 = traj[0]
+        frame0new = trajnew[0]
+        assert (frame0.coords == frame0new.coords) == True
+        print frame0[:10]
+        print frame0new[:10]
+
+        frame1 = traj[1]
+        frame1new = trajnew[1]
+        print frame1[:10]
+        print frame1new[:10]
+        assert (frame1.coords == frame1new.coords) == False
+
+        # try do-fitting from Python
+        # not right yet
+        rmsd, mat, v1, v2 = frame1.rmsd(frame0, get_mvv=True)
+        print rmsd
+        frame1.trans_rot_trans(v1, mat, v2)
+        print frame1[:10]
+        print frame1.rmsd_nofit(frame1new)
+        assert frame1.rmsd(frame1new) < 1E-3
+
+if __name__ == "__main__":
+    unittest.main()
