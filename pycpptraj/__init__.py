a67b7a96b8d5f488b60f306e9a643f749c526458
hainm <hainm.comp@gmail.com>
2015-01-20 17:41:37 -0500

    update pycpptraj/__init__.py

--- pycpptraj/__init__.py
+++ pycpptraj/__init__.py
@@ -13,6 +13,7 @@ from pycpptraj.ArgList import ArgList
 from pycpptraj.AtomMask import AtomMask
 from pycpptraj.CpptrajState import CpptrajState
 from pycpptraj.Trajin_Single import Trajin_Single
+from pycpptraj.Trajectory import Trajectory
 from pycpptraj.trajs.Trajout import Trajout
 from pycpptraj.cast_dataset import cast_dataset
 from pycpptraj.parms.ParmFile import ParmFile
