37392dacbdd807bc8acaacf2e407d8ef0ea17d58
hainm <hainm.comp@gmail.com>
2015-01-20 12:06:46 -0500

    update pycpptraj/utils/check_and_assert.py

--- pycpptraj/utils/check_and_assert.py
+++ pycpptraj/utils/check_and_assert.py
@@ -1,3 +1,19 @@
 def file_exist(filename):
     import os
     return os.path.isfile(filename)
+
+def assert_almost_equal(arr0, arr1, decimals=3):
+    '''numpy-like assert'''
+
+    almost_equal = True
+    SMALL = 10**(-decimals)
+
+    for x, y in zip(arr0, arr1):
+        if abs(x - y) > SMALL:
+            almost_equal = False
+    assert almost_equal == True
+
+if __name__ == "__main__":
+    assert_almost_equal([1., 2., 3.], [1., 2., 3.], decimals=3)
+    assert_almost_equal([1., 2., 3.], [1., 2.,], decimals=3)
+    assert_almost_equal([1., 2., 4.], [1., 2., 3.], decimals=3)
