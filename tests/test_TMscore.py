05420364bfc6ff30c10dc7876406f9c44f2083e1
hainm <hainm.comp@gmail.com>
2015-01-20 00:33:59 -0500

    update tests/test_TMscore.py

--- tests/test_TMscore.py
+++ tests/test_TMscore.py
@@ -10,13 +10,13 @@ class Test(unittest.TestCase):
         # we could generate new traj.top from pdb file too
         #traj.top = Topology("./test_0_after.pdb")
         tm_0 = tm_score(traj[0], traj[1])
-        print tm_0
-        print tm_score(traj[0], traj[0])
-        print tm_score(traj[0], traj[1], mask="!@CA", top=traj.top)
-        print tm_score(traj[0], traj[1], mask1="!:2-10@CA", top1=traj.top,
-                                         mask2="!:2-10@CA", top2=traj.top)
-
-        #assert tm_0 == ?
+        #print tm_0
+        #print tm_score(traj[0], traj[0]) # == 1.
+        #print tm_score(traj[0], traj[1], mask="!@CA", top=traj.top)
+        for idx, frame in enumerate(traj[490:501] + traj[-1:-10:-1]):
+            score = tm_score(frame, traj[0], mask1="!:2-10@CA", top1=traj.top,
+                                             mask2="!:2-10@CA", top2=traj.top)
+            print "%s : %s" % (idx, score)
 
 if __name__ == "__main__":
     unittest.main()
