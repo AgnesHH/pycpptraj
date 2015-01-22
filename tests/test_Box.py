8dce0d0921380e9faed9aff7466fae9c98a47505
hainm <hainm.comp@gmail.com>
2015-01-22 17:44:45 -0500

    update tests/test_Box.py

--- tests/test_Box.py
+++ tests/test_Box.py
@@ -1,6 +1,7 @@
 import unittest
 from pycpptraj.base import *
 from pycpptraj import io as mdio
+from pycpptraj.Box import Box
 from array import array as pyarray
 
 class TestBox(unittest.TestCase):
@@ -8,11 +9,15 @@ class TestBox(unittest.TestCase):
         traj = mdio.load("./data/md1_prod.Tc5b.x", "./data/Tc5b.top")
         frame0 = traj[0]
         frame0.box_crd()
-        print frame0.box
+        print frame0.get_box()
         frame0.boxview[:] = pyarray('d', [0.0, 1.0, 2.0, 3.0, 4.0, 6.])
-        print frame0.box
+        print frame0.get_box()
+        print frame0.get_box().btype
         frame0.set_nobox()
-        print frame0.box
+        print frame0.get_box()
+
+    def test_help(self):
+        Box.help()
 
 if __name__ == "__main__":
     unittest.main()
