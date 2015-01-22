dd838271baab59a47ec6d6f451ef5303b9ffb2ca
hainm <hainm.comp@gmail.com>
2015-01-21 21:51:05 -0500

    update tests/test_get_subframe.py

--- tests/test_get_subframe.py
+++ tests/test_get_subframe.py
@@ -23,8 +23,8 @@ class TestSubFrame(unittest.TestCase):
             farray2.append(frame.get_subframe("@CA", farray.top))
 
         print farray2.size
-        farray2[0][0] = 100.
-        assert farray2[0][0] == 100.
+        farray2[0, 0, 0] = 100.
+        assert farray2[0, 0, 0] == 100.
         assert farray[0][0] != 100.
         print farray[0].atoms(0)
 
