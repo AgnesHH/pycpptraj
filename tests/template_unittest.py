2268fcf3f4f2a4afa85c00d00b681977c5d3e740
hainm <hainm.comp@gmail.com>
2015-01-22 17:36:38 -0500

    update tests/template_unittest.py

--- tests/template_unittest.py
+++ tests/template_unittest.py
@@ -1,6 +1,7 @@
 import unittest
 from pycpptraj.base import *
 from pycpptraj import io as mdio
+from pycpptraj.utils.check_and_assert import assert_almost_equal
 
 class Test(unittest.TestCase):
     def test_0(self):
