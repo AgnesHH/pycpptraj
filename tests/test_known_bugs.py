3473481c7006e3c4d80e0453a98ddb10b0896f19
hainm <hainm.comp@gmail.com>
2015-01-21 23:21:38 -0500

    update tests/test_known_bugs.py

--- tests/test_known_bugs.py
+++ tests/test_known_bugs.py
@@ -3,6 +3,7 @@
 import unittest
 from pycpptraj.base import *
 from pycpptraj import io as mdio
+from pycpptraj.utils.check_and_assert import assert_almost_equal
 import numpy as np
 
 class TestBugs(unittest.TestCase):
@@ -14,6 +15,13 @@ class TestBugs(unittest.TestCase):
 
         # bugs: trajcpp[0, 0, 0] != farray[0, 0, 0] (must be equal)
         assert farray[0][0, 0] == trajcpp[0][0, 0]
+
+        farray[0, 0, 0] = 100.10
+        print farray
+        print farray[0, 0, 0]
+        assert farray[0, 0, 0] == 100.10
+        farray[0, 0, :] = [100.10, 101.1, 102.1]
+        assert_almost_equal(farray[0, 0, :], [100.10, 101.1, 102.1])
 
 if __name__ == "__main__":
     unittest.main()
