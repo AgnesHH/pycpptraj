fa6d747684df182dc3b9eb9850a5ee5f653bc54b
hainm <hainm.comp@gmail.com>
2015-01-23 14:52:51 -0500

    update examples/fancy_indexing_trajectory.py

--- examples/fancy_indexing_trajectory.py
+++ examples/fancy_indexing_trajectory.py
@@ -6,8 +6,8 @@ traj = mdio.load("./data/md1_prod.Tc5b.x", "./data/Tc5b.top")[:]
 print traj[0, 0, 0]
 
 # get new array with shape = (traj.n_frames, n_atoms, 3)
-print traj[:, :, :]
+print traj[:, :, :].shape
 
 # get new array with shape (n_atoms, 3) for 0-th frame
-print traj[0, :, :]
+print traj[0, :, :].shape
 
