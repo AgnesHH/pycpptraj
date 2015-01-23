79ef8445240365b54169f6f882748e6444f2ff5c
hainm <hainm.comp@gmail.com>
2015-01-23 14:37:19 -0500

    update modified:

--- examples/VisitCaseGroup.py
+++
@@ -1,8 +0,0 @@
-from pycpptraj.Trajectory import Trajectory
-
-traj = Trajectory("data/md1_prod.Tc5b.x", "./data/Tc5b.top")
-print traj[:100] + traj[300:500:2] + traj[600:] + traj[500:200:-2] + traj[-1:-15:]
-print traj[-1:-3:-1]
-
-for frame in traj:
-    print frame
