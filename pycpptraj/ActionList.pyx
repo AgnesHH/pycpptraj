36e8b5f92fd88820fa20fb160e0df7e08520c09c
hainm <hainm.comp@gmail.com>
2015-01-23 17:12:01 -0500

    update pycpptraj/ActionList.pyx

--- pycpptraj/ActionList.pyx
+++ pycpptraj/ActionList.pyx
@@ -1,6 +1,8 @@
 # distutils: language = c++
 from cython.operator cimport dereference as deref
 
+# TODO : double-check C++ code
+
 cdef class ActionList:
     def __cinit__(self):
         self.thisptr = new _ActionList()
@@ -11,16 +13,6 @@ cdef class ActionList:
 
     def clear(self):
         self.thisptr.Clear()
-
-    def set_debug(self,int debug):
-        self.thisptr.SetDebug(debug)
-
-    # where?
-    #def SetSilent(self,bint b):
-    #    self.thisptr.SetSilent(b)
-
-    def debug(self):
-        return self.thisptr.Debug()
 
     def add_action(self, actionobj, ArgList arglist, TopologyList toplist, 
                          FrameList flist, DataSetList dlist, DataFileList dflist):
@@ -42,38 +34,32 @@ cdef class ActionList:
                                       toplist.thisptr, flist.thisptr, 
                                       dlist.thisptr, dflist.thisptr)
 
-    # Why put comment those?
-    #def setup_actions(self, TopologyList toplist, int idx):
-    #    cdef _Topology* topptr
-    #    topptr = toplist.thisptr.GetParm(idx)
-    #    return self.thisptr.SetupActions(&topptr)
-
-    def setup_actions(self, Topology top):
+    def process(self, Topology top):
         # let cpptraj free mem
         top.py_free_mem = False
         return self.thisptr.SetupActions(&(top.thisptr))
 
-    def do_actions(self, Frame frame, int idx=0):
+    def do_actions(self, int idx=0, Frame frame):
+        # TODO : read cpptraj code to check memory stuff
+        # set py_free_mem = False to let cpptraj does its job
         frame.py_free_mem = False
         return self.thisptr.DoActions(&(frame.thisptr), idx)
 
-    def print_list(self):
-        self.thisptr.Print()
-
-    def list(self):
+    def listinfo(self):
         self.thisptr.List()
 
-    def empty(self):
+    def is_empty(self):
         return self.thisptr.Empty()
 
     @property
     def n_actions(self):
         return self.thisptr.Naction()
 
-    def cmd_string(self,int i):
+    def cmd_string(self, int i):
         return self.thisptr.CmdString(i)
 
     def action_alloc(self, int i):
+        # TODO : do we need to expose this method here?
         # return func_ptr
         cdef FunctPtr func = FunctPtr()
         if i >= self.n_actions:
