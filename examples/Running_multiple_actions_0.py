ac390688d570b530ec38d41669ca47f509606154
hainm <hainm.comp@gmail.com>
2015-01-23 17:27:38 -0500

    update examples/Running_multiple_actions_0.py

--- examples/Running_multiple_actions_0.py
+++ examples/Running_multiple_actions_0.py
@@ -3,6 +3,7 @@ from time import time
 from pycpptraj.base import *
 from pycpptraj import allactions
 from pycpptraj import io as mdio
+from pycpptraj.utils.check_and_assert import assert_almost_equal
 
 def run_0():
     # load traj
@@ -27,15 +28,19 @@ def run_0():
     # add two actions: Action_Strip and Action_Distance
     alist.add_action(stripact, ArgList("@H*"), toplist, None, dsetlist, dflist)
     alist.add_action(allactions.Action_Distance(), ArgList(":2@CA :3@CA"), toplist, None, dsetlist, dflist)
+    alist.add_action(allactions.Action_Rmsd(), ArgList("rms first @CA"), toplist, None, dsetlist, dflist)
 
     # 
     print "test setup_actions"
-    print alist.n_actions
+    print "number of actions = ", alist.n_actions
 
     # do checking
     alist.process(toplist[0])
 
     farray2 = FrameArray()
+
+    # creat frame0 as data holder to avoid free memory twices
+    # TODO : fix this
     frame0 = Frame()
     # testing how fast to do the actions
 
@@ -60,8 +65,19 @@ def run_0():
     assert farray2[0].n_atoms != farray[0].n_atoms
 
     # it's time to retrieve the data
-    ds = cast_dataset(dsetlist[0], dtype='general')
-    print ds[:10]
+    # get distance data
+    ds0 = cast_dataset(dsetlist[0], dtype='general')
+    # get rmsd data
+    ds1 = cast_dataset(dsetlist[1], dtype='general')
+    print ds0[:10]
+    print ds1[:10]
+
+    # reproduce cpptraj's output?
+    import numpy as np
+    rmsdcpp = np.loadtxt("./data/rmsd_to_firstFrame_CA_allres.Tc5b.dat", skiprows=1).transpose()[1][:10]
+    # YES
+    assert_almost_equal(rmsdcpp, ds1[:10])
+    help(cast_dataset)
 
 if __name__ == "__main__":
     run_0()
