0ecae4c2846d3f8685cc14b6eb4b4f05a907cf74
hainm <hainm.comp@gmail.com>
2015-01-23 16:57:44 -0500

    update tests/test_ActionList.py

--- tests/test_ActionList.py
+++ tests/test_ActionList.py
@@ -1,57 +1,41 @@
-from copy import copy
+import unittest
+from time import time
 from pycpptraj.base import *
 from pycpptraj import allactions
+from pycpptraj import io as mdio
 
-top = Topology("./data/Tc5b.top")
-ref = ReferenceFrame()
-ref.load_ref("./data/Tc5b.nat.crd", top)
-refframe = ref.frame
-mdx = "./data/md1_prod.Tc5b.x"
+class TestActionList(unittest.TestCase):
+    
+    print "test ActionList"
+    farray = mdio.load("./data/md1_prod.Tc5b.x", "./data/Tc5b.top")
+    stripact = allactions.Action_Strip()
+    dsetlist = DataSetList()
+    dflist = DataFileList()
+    alist = ActionList()
+    toplist = TopologyList()
+    toplist.add_parm(farray.top)
+    alist.add_action(stripact, ArgList("@H*"), toplist, None, dsetlist, dflist)
+    alist.add_action(allactions.Action_Distance(), ArgList(":2@CA :3@CA"), toplist, None, dsetlist, dflist)
+    print "test setup_actions"
+    print alist.n_actions
+    alist.setup_actions(toplist[0])
+    alist.setup_actions(farray.top.copy())
 
-farray = FrameArray()
-traj = Trajin_Single()
-traj.load(mdx, top)
-frame = Frame()
-frame.set_frame_v(top, traj.has_vel(), traj.n_repdims)
-atm = AtomMask(":1-20@CA")
+    t0 = time()
+    farray2 = FrameArray()
+    frame0 = Frame()
+    for frame in farray:
+        alist.do_actions(frame)
+        farray2.append(frame)
+    print time() - t0
 
-input2 = """
-reference ./data/Tc5b.nat.crd
-distance d1 :2@CA :10@CA
-rmsd reference out test.out :1-20@CA
-"""
+    print farray2.size
+    print farray2.n_frames
+    print farray2[0].n_atoms
+    print farray[0].n_atoms
 
-arglist = ArgList(input2)
-traj.load(mdx, top, arglist)
-alist = ActionList()
-armsd = allactions.Action_Rmsd()
-distact = allactions.Action_Distance()
-toplist = TopologyList()
-toplist.add_parm("./data/Tc5b.top")
-framelist = FrameList()
+    ds = cast_dataset(dsetlist[0], dtype='general')
+    print ds[:10]
 
-# add to farray
-traj.begin_traj(1)
-for i in range(10):
-    traj.read_traj_frame(i, frame)
-    farray.append(frame)
-traj.end_traj()
-
-print "test ActionList"
-stripact = allactions.Action_Strip()
-dsetlist = DataSetList()
-dflist = DataFileList()
-alist.add_action(stripact, ArgList("@H*"), toplist, framelist, dsetlist, dflist)
-print "test setup_actions"
-print alist.n_actions
-alist.setup_actions(toplist[0])
-alist.setup_actions(top)
-print top.n_atoms
-print toplist[0].n_atoms
-print "farray[0].n_atoms = ", farray[0].n_atoms
-alist.do_actions(farray[0])
-print "farray[0].n_atoms = ", farray[0].n_atoms
-frame0 = farray[0]
-print "frame0.n_atoms = ", frame0.n_atoms
-alist.do_actions(frame0)
-print "frame0.n_atoms = ", frame0.n_atoms
+if __name__ == "__main__":
+    unittest.main()
