04da02ac8be69126f598356c9aa0c274f26872eb
hainm <hainm.comp@gmail.com>
2015-01-24 02:38:59 -0500

    update tests/test_memview3D.py

--- tests/test_memview3D.py
+++ tests/test_memview3D.py
@@ -9,6 +9,9 @@ class Test(unittest.TestCase):
         traj = mdio.load("./data/md1_prod.Tc5b.x", "./data/Tc5b.top")[:]
         arr0 = traj[:, :, :]
         print arr0.shape
+        arr0[0, 0, 0] = 105.
+        print traj
+        print traj[0, 0, 0]
         # TODO : return (n_frames, n_atoms, 3) array as a view of traj
 
 if __name__ == "__main__":
