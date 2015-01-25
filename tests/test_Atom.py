eccb23d055153060103a392f167a70a2965b0eb2
hainm <hainm.comp@gmail.com>
2015-01-24 20:09:43 -0500

    update tests/test_Atom.py

--- tests/test_Atom.py
+++ tests/test_Atom.py
@@ -17,7 +17,6 @@ class Test(unittest.TestCase):
         print len(a0.element_short_name)
         print a0.atomic_number
         print dir(a0)
-        print a0.get_bond_length(1, 7)
         print a0.molnum
         print a0.nbonds
         print a0.typeindex
@@ -30,8 +29,19 @@ class Test(unittest.TestCase):
         
         top = Topology("./data/HP36.top")
         a0 = top.atomlist[0]
-        a1 = a0.copy()
-        print a1
+        a100 = top.atomlist[100]
+        a0cp = a0.copy()
+        print a0
+        print a0cp
+        print a100
+        assert a0.resnum == 0
+        print a100.resnum
+        print dir(a100)
+        print a100.mass
+        print a100.name
+        print len(a100.name)
+        print a100.is_bonded_to(120)
+        print a100.n_bonds
 
 if __name__ == "__main__":
     unittest.main()
