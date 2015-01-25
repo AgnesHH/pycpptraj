60fc1cecbca2d637e680b4b01aa51c8e33c94396
hainm <hainm.comp@gmail.com>
2015-01-24 22:47:52 -0500

    update tests/TestListTravis.sh

--- tests/TestListTravis.sh
+++ tests/TestListTravis.sh
@@ -27,6 +27,7 @@ python ./test_Topology.py
 python ./test_TrajectoryFile.py 
 python ./test_ParmFile.py 
 python ./test_iterload.py 
+python ./test_ActionList_Image.py 
 python ./test_TrajReadOnly.py 
 python ./test_AtomMask.py 
 python ./test_load_indices.py 
@@ -36,6 +37,7 @@ python ./test_Vec3.py
 python ./test_GridAction.py 
 python ./test_ActionList.py 
 python ./test_DistRoutines.py 
+python ./test_Action_Image.py 
 python ./test_memview3D.py 
 python ./test_writepdb.py 
 python ./test_indexing_speed.py 
@@ -51,6 +53,7 @@ python ./test_Frame_4.py
 python ./test_TopologyList.py 
 python ./test_DataSetList.py 
 python ./test_loadpdb_rcsb.py 
+python ./test_Atom.py 
 python ./test_Trajin.py 
 python ./test_Frame_1.py 
 python ./test_FrameArray2.py 
@@ -60,3 +63,4 @@ python ./test_fancy_indexing_simple_test.py
 python ./test_loadingtime.py 
 python ./test_Frame_2.py 
 python ./test_has_numpy.py 
+python ./test_Atom_2.py 
