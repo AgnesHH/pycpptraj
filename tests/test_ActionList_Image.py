82fb39212eee9de42f64fc3a26b2a4bdbb5ab827
hainm <hainm.comp@gmail.com>
2015-01-25 04:48:56 -0500

    update tests/test_ActionList_Image.py

--- tests/test_ActionList_Image.py
+++ tests/test_ActionList_Image.py
@@ -3,11 +3,20 @@ from time import time
 from pycpptraj.base import *
 from pycpptraj import allactions
 from pycpptraj import io as mdio
+from pycpptraj.utils.check_and_assert import file_exist
 
 class TestActionList(unittest.TestCase):
     def test_run_0(self):
         # load traj
-        farray = mdio.load("./data/md1_prod.Tc5b.x", "./data/Tc5b.top")
+        farray = FrameArray(filename="./data/tz2.truncoct.nc", top="./data/tz2.truncoct.parm7")[:2]
+        fold = farray.copy()                                              
+        print "old file: ", fold[0, 0, :]
+        
+        act = allactions.Action_Image()                                   
+        ptrajin = """                                                     
+        center :2-11
+        image center familiar com :6                                      
+        """              
     
         # create 'strip' action
         stripact = allactions.Action_Strip()
@@ -26,12 +35,11 @@ class TestActionList(unittest.TestCase):
         toplist.add_parm(farray.top)
     
         # add two actions: Action_Strip and Action_Distance
-        alist.add_action(stripact, ArgList("@H*"), toplist, None, dsetlist, dflist)
-        alist.add_action(allactions.Action_Distance(), ArgList(":2@CA :3@CA"), toplist, None, dsetlist, dflist)
+        alist.add_action(allactions.Action_Image(), ArgList("center :2-11"), top=toplist)
+        alist.add_action(allactions.Action_Image(), ArgList("center familiar com :6"), top=toplist)
     
         # 
-        print "test setup_actions"
-        print alist.n_actions
+        assert alist.n_actions == 2
     
         # do checking
         alist.process(toplist[0])
@@ -40,12 +48,12 @@ class TestActionList(unittest.TestCase):
         frame0 = Frame()
         # testing how fast to do the actions
     
-        t0 = time()
         # loop all frames
         # use iterator to make faster loop
         # don't use "for i in range(farray.size)"
         for frame in farray:
             # perform actions for each frame
+            # we make a copy since we want to keep orginal Frame
             frame0 = frame.copy()
             alist.do_actions(frame=frame0)
             #alist.do_actions(frame)
@@ -53,16 +61,25 @@ class TestActionList(unittest.TestCase):
             # we need to keep the modified frame in farray2
             #farray2.append(frame)
             farray2.append(frame0)
-        print time() - t0
     
         # make sure that Action_Strip does its job in stripping
         print farray2.size
         assert farray2.n_frames == farray.n_frames
-        assert farray2[0].n_atoms != farray[0].n_atoms
-    
-        # it's time to retrieve the data
-        ds = cast_dataset(dsetlist[0], dtype='general')
-        print ds[:10]
 
+        if file_exist("./CpptrajTest/Test_Image/image4.crd"):
+            fnew = mdio.load("./CpptrajTest/Test_Image/image4.crd", 
+                             "./data/tz2.truncoct.parm7")
+            assert fnew.size == 2
+            # make sure that pycpptraj reproduce cpptraj outputo
+            # TODO : not yet. double-check
+            print fnew[0].same_coords_as(farray2[0])
+            print farray[0].same_coords_as(farray2[0])
+            print fnew[0, 0, :]
+            print farray[0, 0, :]
+            print farray2[0, 0, :]
+            print fold[0].same_coords_as(farray2[0])
+            print fnew[0].rmsd(farray2[0])
+
+    
 if __name__ == "__main__":
     unittest.main()
