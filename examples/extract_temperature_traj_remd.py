38b51155785a15ba1d0c1f6522c216ebc038156f
hainm <hainm.comp@gmail.com>
2015-01-25 05:45:22 -0500

    update examples/extract_temperature_traj_remd.py

--- examples/extract_temperature_traj_remd.py
+++ examples/extract_temperature_traj_remd.py
@@ -6,54 +6,54 @@ to write new script with pycpptraj
 
 import unittest
 from array import array
-from sets import Set
 from glob import glob
 from pycpptraj.base import *
 from pycpptraj import io as mdio
 from pycpptraj.utils.check_and_assert import assert_almost_equal
 
-class Test(unittest.TestCase):
-    def test_0(self):
-       # create a list of all remd trajs
-       flist = glob("../tests/data/Test_RemdTraj/rem.nc.*") 
+def get_frames_same_T():
+   # create a list of all remd trajs
+   flist = glob("../tests/data/Test_RemdTraj/rem.nc.*") 
 
-       # make a list of TrajReadOnly instances
-       trajlist = []
-       for fh in flist:
-           topfile = "../tests/data/Test_RemdTraj/ala2.99sb.mbondi2.parm7"
-           trajlist.append(mdio.load(fh, topfile))
+   # make a list of TrajReadOnly instances
+   trajlist = []
+   for fh in flist:
+       topfile = "../tests/data/Test_RemdTraj/ala2.99sb.mbondi2.parm7"
 
+       # load trajectory and append to trajlist
+       trajlist.append(mdio.load(fh, topfile))
 
-       # make FrameArray instance that holds 492.2 T frames
-       # we need to reserve n_frames to hold the data
-       f4922 = FrameArray(n_frames=trajlist[0].n_frames)
+   # make FrameArray instance that holds 492.2 T frames
+   # we need to reserve n_frames to hold the data
+   f4922 = FrameArray(n_frames=trajlist[0].n_frames)
 
-       assert f4922.n_frames == trajlist[0].n_frames
-       f4922.top = trajlist[0].top.copy()
+   assert f4922.n_frames == trajlist[0].n_frames
+   f4922.top = trajlist[0].top.copy()
 
-       for traj in trajlist:
-           for idx, frame in enumerate(traj):
-               if frame.temperature == 492.2:
-                   f4922[idx] = frame
+   # extract frames having T = 492.2
+   # use iteration for nested loops
+   for traj in trajlist:
+       for idx, frame in enumerate(traj):
+           if frame.temperature == 492.2:
+               # we don't use `append` method since we want to make sure 
+               # frames are in the order of simulation time
+               f4922[idx] = frame
 
-       # make sure f4922 only hold frames having T = 492.2 K
-       arr0 = array('d', [492.2, 492.2, 492.2, 492.2, 492.2, 
-                         492.2, 492.2, 492.2, 492.2, 492.2])
-       assert f4922.temperatures == arr0
+   # make sure f4922 only hold frames having T = 492.2 K
+   arr0 = array('d', [492.2, 492.2, 492.2, 492.2, 492.2, 
+                     492.2, 492.2, 492.2, 492.2, 492.2])
+   assert f4922.temperatures == arr0
 
-       # make sure we reproduce cpptraj output
-       cpptraj = mdio.load("../tests/data/Test_RemdTraj/temp0.crd.492.20", topfile)
+   # make sure we reproduce cpptraj output
+   cpptraj = mdio.load("../tests/data/Test_RemdTraj/temp0.crd.492.20", topfile)
+   for idx, framepy in enumerate(f4922):
+       assert_almost_equal(framepy.coords, cpptraj[idx].coords)
+       print "rmsd between pycpptraj Frame and cpptraj Frame = %s " % framepy.rmsd(cpptraj[idx])
 
-       for idx, framepy in enumerate(f4922):
-           assert_almost_equal(framepy.coords, cpptraj[idx].coords)
+   print f4922[5].coords[:10]
+   print cpptraj[5].coords[:10]
 
-
-           print "rmsd between pycpptraj Frame and cpptraj Frame = %s " % framepy.rmsd(cpptraj[idx])
-
-       print f4922[5].coords[:10]
-       print cpptraj[5].coords[:10]
-
-       print "YES"
+   print "YES, we can reproduce cpptraj output"
 
 if __name__ == "__main__":
-    unittest.main()
+    get_frames_same_T()
