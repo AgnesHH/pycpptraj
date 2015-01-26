6d138b6e231f07eb560cad17fd79736381a73c59
hainm <hainm.comp@gmail.com>
2015-01-25 21:20:57 -0500

    update tests/test_Vec3.py

--- tests/test_Vec3.py
+++ tests/test_Vec3.py
@@ -7,6 +7,17 @@ class Test(unittest.TestCase):
     def test_1(self):
         # FIXME : segfault
         v1 = Vec3(0., 0., 0.)
+        print v1[:]
+        v1[0] = 200.
+        v1[:] = [100, 300, 400]
+        assert v1.tolist() == [100, 300, 400]
+        assert isinstance(v1.tolist(), list) == True
+        print np.asarray(v1[:])
+        print v1
+
+        from pycpptraj.math import Matrix_3x3
+        mat = Matrix_3x3(range(9))
+        print mat*v1
 
 if __name__ == "__main__":
     unittest.main()
