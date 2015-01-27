bc21e26eb6134e0f261f671c13456ec423f7faab
hainm <hainm.comp@gmail.com>
2015-01-26 20:43:38 -0500

    update tests/test_GDTscore.py

--- tests/test_GDTscore.py
+++ tests/test_GDTscore.py
@@ -4,33 +4,29 @@ from array import array
 from pycpptraj.base import *
 from pycpptraj import AtomSelect
 from pycpptraj import io as mdio
+from pycpptraj.decorators import no_test
 from pycpptraj.utils.check_and_assert import assert_almost_equal
-from TestGDTscore.gdt import gdt
-from TestGDTscore.calc_gdt import calc_GDTscore
+from pycpptraj.common_actions import calc_score
 
 class Test(unittest.TestCase):
     def test_0(self):
+        IDX0 = 99
+        IDX1 = 98
         traj = mdio.load("./data/md1_prod.Tc5b.x", "./data/Tc5b.top")
-        #mdio.writetraj(filename="test_gdt_0.pdb", traj=traj[0], top=traj.top)
-        #mdio.writetraj(filename="test_gdt_1.pdb", traj=traj[1], top=traj.top)
-        print calc_GDTscore(traj[0], traj[1], "@CA", traj.top, score='tmscore')/1000.
-        print calc_GDTscore(traj[1], traj[0], "@CA", traj.top, score='tmscore')/1000.
+        # write pdb files for TMalign program so we can compare our result to TMalign
+        # ./TMalign -A test_gdt_0.pdb -B test_gdt_1.pdb
+        mdio.writetraj(filename="test_gdt_0.pdb", traj=traj[IDX0], top=traj.top, overwrite=True)
+        mdio.writetraj(filename="test_gdt_1.pdb", traj=traj[IDX1], top=traj.top, overwrite=True)
 
-    def test_1(self):
-        traj0 = mdio.load(filename="./test_wrap_TMalign/TM/PDB1.pdb", 
-                top="./test_wrap_TMalign/TM/PDB1.pdb")[:]
-        traj1 = mdio.load(filename="./test_wrap_TMalign/TM/PDB2.pdb", 
-                top="./test_wrap_TMalign/TM/PDB2.pdb")[:]
+        # do our calculation
+        # score = 'gdtscore', 'tmscore' or 'maxsub'
+        score = 'tmscore'
+        print '%s = %s ' % (score, calc_score(traj[IDX0], traj[IDX1], "@CA", traj.top, score=score)/1000.)
+        print calc_score(traj[IDX1], traj[IDX0], "@CA", traj.top, score=score)/1000.
 
-        print traj0[0].n_atoms
-        print traj1[0].n_atoms
-
-        traj0.strip_atoms(mask="!@CA", update_top=True)
-        traj1.strip_atoms(mask="!@CA", update_top=True)
-
-        print traj0[0].n_atoms
-        print traj1[0].n_atoms
+        # calculate RMSD
+        print "rmsd = ", traj[IDX0].rmsd(traj[IDX1])
 
 if __name__ == "__main__":
-    #unittest.main()
-    pass
+    unittest.main()
+    #pass
