73b9c5f19d5f6b81ccdb44c0f99d5af4d4c0b924
hainm <hainm.comp@gmail.com>
2015-01-27 04:02:15 -0500

    add test_FrameArray_2.py test_hd5f.py

--- tests/test_hd5f.py
+++ tests/test_hd5f.py
@@ -3,26 +3,23 @@ from pycpptraj.base import *
 from pycpptraj import io as mdio
 from pycpptraj.trajs.Traj_HD5F import HD5F
 from pycpptraj.utils.check_and_assert import assert_almost_equal
+from pycpptraj.decorators import no_test
+from pycpptraj.load_HD5F import load_hd5f
 import numpy as np
 
 # note : API will be changed. 
 class Test(unittest.TestCase):
+    @no_test
     def test_0(self):
-        pass
-        #h5 = HD5F()
+        h5 = HD5F()
+        # FIXME : *** Error in `python': double free or corruption
         #h5.load("./data/ala2.h5")
+        #print h5
+        #print h5[0]
 
     def test_1(self):
-        farray = FrameArray()
-        import h5py 
-        fh = h5py.File("./data/ala2.h5", 'r')
-        crd = fh['coordinates']
-        shape = crd.shape
-
-        farray.resize(shape[0])
-        for idx, arr in enumerate(crd):
-            farray[idx] = Frame(shape[1])
-            farray[idx].set_from_crd(arr.flatten().astype(np.float64))
+        traj = load_hd5f("./data/ala2.h5")
+        print traj
 
 if __name__ == "__main__":
     unittest.main()
