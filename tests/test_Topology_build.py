613f8f356f68da9a31a4143e4085f46e41f37a89
hainm <hainm.comp@gmail.com>
2015-01-27 06:05:10 -0500

    update tests/test_Topology_build.py

--- tests/test_Topology_build.py
+++ tests/test_Topology_build.py
@@ -1,13 +1,15 @@
 import unittest
 from pycpptraj.base import *
+from pycpptraj import NameType
 from pycpptraj import io as mdio
 from pycpptraj.utils.check_and_assert import assert_almost_equal
 
 class Test(unittest.TestCase):
     def test_0(self):
         top = Topology()
-        top.add_atom(Atom("H3D", ' ', 0), 1, NameType("H3D"), 0)
+        top.add_atom(Atom("H3D", ' ', 0), 1, NameType("H3D"))
         print top.n_atoms
+        print top[0]
 
 if __name__ == "__main__":
     unittest.main()
