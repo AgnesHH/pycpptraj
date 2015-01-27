340e4cfc7d759c0cc6b120561591de5c79a20fac
hainm <hainm.comp@gmail.com>
2015-01-27 06:23:44 -0500

    update tests/test_Topology_build.py

--- tests/test_Topology_build.py
+++ tests/test_Topology_build.py
@@ -7,9 +7,13 @@ from pycpptraj.utils.check_and_assert import assert_almost_equal
 class Test(unittest.TestCase):
     def test_0(self):
         top = Topology()
-        top.add_atom(Atom("H3D", ' ', 0), 1, NameType("H3D"))
-        print top.n_atoms
-        print top[0]
+        atom = Atom(NameType("H3D"), NameType(""))
+        top.add_atom(atom=atom, resid=1, resname="H3D")
+        top.add_atom(atom=atom, resid=0, resname="H3D")
+        top.add_atom(atom=atom, resid=0, resname="H3D")
+        top.add_atom(atom=atom, resid=0, resname="H3D")
+        top.add_atom(atom=atom, resid=0, resname="H3D")
+        top.bond_info()
 
 if __name__ == "__main__":
     unittest.main()
