c6532ed82f051ddecc78d98938a570230e8da42e
hainm <hainm.comp@gmail.com>
2015-01-27 00:25:46 -0500

    update tests/test_DataSetList.py

--- tests/test_DataSetList.py
+++ tests/test_DataSetList.py
@@ -1,6 +1,7 @@
 import unittest
 from pycpptraj.base import *
 from pycpptraj.datasets.DataSet import DataSet
+from pycpptraj.DataFile import DataFile
 
 class TestDataSetList(unittest.TestCase):
     def test_1(self):
@@ -12,7 +13,8 @@ class TestDataSetList(unittest.TestCase):
         self.assertRaises(ValueError, lambda: dlist[0])
 
     def tearDown(self):
-        print "done test"
+        dfile = DataFile()
+        print dfile.dtype
 
 if __name__ == '__main__':
     unittest.main()
