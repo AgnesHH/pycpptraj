d5df35280b32ae260d5f709ae78419af740118d6
hainm <hainm.comp@gmail.com>
2015-01-27 08:44:52 -0500

    update tests/pycpptraj_ipython.py

--- tests/pycpptraj_ipython.py
+++ tests/pycpptraj_ipython.py
@@ -1,3 +1,5 @@
 from pycpptraj import *
 
-TRAJ = FrameArray(filename="./data/md1_prod.Tc5b.x", top="./data/Tc5b.top")
+traj = FrameArray(filename="./data/md1_prod.Tc5b.x", top="./data/Tc5b.top")
+top = traj.top
+frame0 = traj[0]
