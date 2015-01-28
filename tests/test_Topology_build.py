c02310d344871b5a505c12f33f648871c2b41af3
hainm <hainm.comp@gmail.com>
2015-01-27 21:14:30 -0500

    update tests/test_Topology_build.py

--- tests/test_Topology_build.py
+++ tests/test_Topology_build.py
@@ -8,12 +8,13 @@ class Test(unittest.TestCase):
     def test_0(self):
         top = Topology()
         atom = Atom("H3D", "CA")
-        top.add_atom(atom=atom, resid=1, resname="H3D")
         top.add_atom(atom=atom, resid=0, resname="H3D")
         top.add_atom(atom=atom, resid=0, resname="H3D")
         top.add_atom(atom=atom, resid=0, resname="H3D")
         top.add_atom(atom=atom, resid=0, resname="H3D")
-        #top.bond_info()
+        top.add_atom(atom=atom, resid=0, resname="H3D")
+        top.summary()
+        print top.get_atom_indices("@H=")
 
 if __name__ == "__main__":
     unittest.main()
